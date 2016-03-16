4b3a75cc4d80a201c6b9580a5c03d03836793857
hnvarambhia <h.n.varambhia@gmail.com>
2016-03-16 15:15:54 +0000

    We store the title of the article in the CSV row

--- spec/article_csv_row_spec.rb
+++ spec/article_csv_row_spec.rb
@@ -1,14 +1,22 @@
 require 'csv'
 describe 'Article CSV row' do
   def render article
-    [article.doi]
+    [article.doi, article.title]
   end
 
   it 'stores the DOI' do
     expected = '10.1234/altmetric0'
-    article = double :article
-
+    article = double(:article).as_null_object
+    
     expect(article).to receive(:doi).and_return expected     
+    expect(render(article)).to include expected
+  end
+
+  it 'stores the title' do
+    expected = 'Chemistry article'
+    article = double(:article).as_null_object
+
+    expect(article).to receive(:title).and_return expected
     expect(render(article)).to include expected
   end
 end
