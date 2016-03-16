85b344fe5c2ce4b9626f81a97ff6dd478543baf2
hnvarambhia <h.n.varambhia@gmail.com>
2016-03-16 13:54:08 +0000

    The JSON doc stores the article's DOI - faked till we make it

--- spec/article_json_doc_spec.rb
+++ spec/article_json_doc_spec.rb
@@ -1,5 +1,14 @@
-describe '1 + 1' do
-  it 'equals 2' do
-    expect(1 + 1).to eq 2
+describe 'Article JSON Doc' do
+  def render article
+   {'doi' => '10.1234/altmetric0'}
+  end
+
+  it 'stores the DOI' do
+    expected = '10.1234/altmetric0'
+    article = double :article
+    allow(article).to receive(:doi).and_return expected
+    article_json_doc = render article
+
+    expect(article_json_doc['doi']).to eq expected
   end
 end
