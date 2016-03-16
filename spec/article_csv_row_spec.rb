506afbcc90fd0408054b6b562d72b6fde33845d9
hnvarambhia <h.n.varambhia@gmail.com>
2016-03-16 15:27:34 +0000

    We assert that DOI is in the first column

--- spec/article_csv_row_spec.rb
+++ spec/article_csv_row_spec.rb
@@ -8,8 +8,10 @@ describe 'Article CSV row' do
     expected = '10.1234/altmetric0'
     article = double(:article).as_null_object
     
-    expect(article).to receive(:doi).and_return expected     
-    expect(render(article)).to include expected
+    expect(article).to receive(:doi).and_return expected
+    row = render article     
+    expect(row).to include expected
+    expect(row.index(expected)).to eq 0
   end
 
   it 'stores the title' do
