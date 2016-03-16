a2ef3119da1df391b08a8604c9d849d54500f81e
hnvarambhia <h.n.varambhia@gmail.com>
2016-03-16 22:39:17 +0000

    We store the name of the journal in the 4th column

--- spec/article_csv_row_spec.rb
+++ spec/article_csv_row_spec.rb
@@ -1,7 +1,7 @@
 require 'csv'
 describe 'Article CSV row' do
   def render article
-    [article.doi, article.title, article.author]
+    [article.doi, article.title, article.author, article.journal]
   end
 
   it 'stores the DOI in the 1st column' do
@@ -31,6 +31,14 @@ describe 'Article CSV row' do
     expect(row).to have(expected).in_column 3
   end
 
+  it 'stores the journal in the 4th column' do
+    expected = 'Journal of Physics B'
+    article = double(:article).as_null_object
+
+    expect(article).to receive(:journal).and_return expected
+    expect(render(article)).to have(expected).in_column 4
+  end
+
   RSpec::Matchers.define :have do |expected|
     match do |row|
       row.include?(expected) and row.index(expected) == @expected_index
@@ -41,7 +49,8 @@ describe 'Article CSV row' do
     end
 
     failure_message do |row|
-      "Expected #{row} to include '#{expected}' in column #{@expected_index}"
+      column = @expected_index + 1
+      "Expected #{row} to include '#{expected}' in column #{column}"
     end
   end
 end
