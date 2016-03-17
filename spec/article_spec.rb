3a1878d20cc6102a9fe57f303d2a90cafef29761
hnvarambhia <h.n.varambhia@gmail.com>
2016-03-17 21:21:12 +0000

    Forgot to update the tests

--- spec/article_spec.rb
+++ spec/article_spec.rb
@@ -15,28 +15,28 @@ describe 'An article' do
 
   it 'stores its DOI' do
     doi = '10.1234/altmetric0'
-    article = Article.new(doi, nil)
+    article = Article.new(doi, nil, nil)
     
     expect(article.doi).to eq doi
   end
 
   it 'stores any DOI passed to it' do
     doi = '10.1234/altmetric123'
-    article = Article.new(doi, nil)
+    article = Article.new(doi, nil, nil)
 
     expect(article.doi).to eq doi
   end
 
   it 'stores the title' do
     title = 'Electron Scattering by HCN'
-    article = Article.new(nil, title)
+    article = Article.new(nil, title, nil)
 
     expect(article.title).to eq title
   end
 
   it 'stores any title passed to it' do
     title = 'Chemistry Article'
-    article = Article.new(nil, title)
+    article = Article.new(nil, title, nil)
 
     expect(article.title).to eq title
   end 
