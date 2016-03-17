24765d92b52fa50f8140e26332fa3e9b65ff0ed6
hnvarambhia <h.n.varambhia@gmail.com>
2016-03-17 11:29:08 +0000

    Invoke the method directly

--- spec/article_json_doc_spec.rb
+++ spec/article_json_doc_spec.rb
@@ -12,31 +12,25 @@ describe 'Article JSON Doc' do
   it 'stores the DOI' do
     doi = '10.1234/altmetric0'
     article = double(:article).as_null_object
+
     expect(article).to receive(:doi).and_return doi
-
-    article_json_doc = render article
-
-    expect(article_json_doc['doi']).to eq doi
+    expect(render(article)['doi']).to eq doi
   end
 
   it 'stores the title' do
     title = 'Physics article'
     article = double(:article).as_null_object
+
     expect(article).to receive(:title).and_return title
-
-    article_json_doc = render article
-
-    expect(article_json_doc['title']).to eq title
+    expect(render(article)['title']).to eq title
   end
 
   it 'stores the author' do
     author = 'Physicists'
     article = double(:author).as_null_object
+
     expect(article).to receive(:author).and_return author
-
-    article_json_doc = render article
-
-    expect(article_json_doc['author']).to eq author
+    expect(render(article)['author']).to eq author
   end
 
   describe 'an article with multiple authors' do
@@ -52,20 +46,16 @@ describe 'Article JSON Doc' do
   it 'stores the ISSN' do
     issn = '1234-5678'
     article = double(:article).as_null_object
-    expect(article).to receive(:issn).and_return issn
-  
-    article_json_doc = render article
- 
-    expect(article_json_doc['issn']).to eq issn
+
+    expect(article).to receive(:issn).and_return issn  
+    expect(render(article)['issn']).to eq issn
   end
 
   it 'stores the journal' do
     journal = 'Journal of Physics B'
     article = double(:article).as_null_object
+
     expect(article).to receive(:journal).and_return journal
-
-    article_json_doc = render article
-
-    expect(article_json_doc['journal']).to eq journal
+    expect(render(article)['journal']).to eq journal
   end
 end
