efb159e679cf8c073c84f87cff311986bb311f12
hnvarambhia <h.n.varambhia@gmail.com>
2016-03-17 17:14:14 +0000

    Triangulation - the article now stores title passed to its constructor

--- spec/article_spec.rb
+++ spec/article_spec.rb
@@ -1,33 +1,38 @@
 describe 'An article' do
   class Article
-    attr_reader :doi
+    attr_reader :doi, :title
 
-    def initialize(doi)
+    def initialize(doi, title)
       @doi = doi 
-    end
-
-    def title
-      'Electron Scattering by HCN'
+      @title = title
     end
   end
 
   it 'stores its DOI' do
     doi = '10.1234/altmetric0'
-    article = Article.new(doi)
+    article = Article.new(doi, nil)
     
     expect(article.doi).to eq doi
   end
 
   it 'stores any DOI passed to it' do
     doi = '10.1234/altmetric123'
-    article = Article.new(doi)
+    article = Article.new(doi, nil)
 
     expect(article.doi).to eq doi
   end
 
   it 'stores the title' do
     title = 'Electron Scattering by HCN'
-    article = Article.new(nil)
+    article = Article.new(nil, title)
+
     expect(article.title).to eq title
   end
+
+  it 'stores any title passed to it' do
+    title = 'Chemistry Article'
+    article = Article.new(nil, title)
+
+    expect(article.title).to eq title
+  end 
 end
