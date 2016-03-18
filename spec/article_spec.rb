7ced598938a461ff2971aba0056558d73ee462d5
Hemal N Varambhia <h.n.varambhia@gmail.com>
2016-03-18 18:17:38 +0000

    The article stores the ISSN that is passed to it

--- spec/article_spec.rb
+++ spec/article_spec.rb
@@ -2,55 +2,56 @@ describe 'An article' do
   class Article
     attr_reader :doi, :title, :author
 
-    def initialize(doi, title, author)
+    def initialize(doi, title, author, issn)
       @doi = doi 
       @title = title
       @author = author
+      @issn = issn
     end
 
     def issn
-      '1234-5678'
+      @issn
     end
   end
 
   it 'stores its DOI' do
     doi = '10.1234/altmetric0'
-    article = Article.new(doi, nil, nil)
+    article = Article.new(doi, nil, nil, nil)
     
     expect(article.doi).to eq doi
   end
 
   it 'stores any DOI passed to it' do
     doi = '10.1234/altmetric123'
-    article = Article.new(doi, nil, nil)
+    article = Article.new(doi, nil, nil, nil)
 
     expect(article.doi).to eq doi
   end
 
   it 'stores the title' do
     title = 'Electron Scattering by HCN'
-    article = Article.new(nil, title, nil)
+    article = Article.new(nil, title, nil, nil)
 
     expect(article.title).to eq title
   end
 
   it 'stores any title passed to it' do
     title = 'Chemistry Article'
-    article = Article.new(nil, title, nil)
+    article = Article.new(nil, title, nil, nil)
 
     expect(article.title).to eq title
   end 
 
   it 'stores the author' do
     author = ['Physicist']
-    article = Article.new(nil, nil, author)
+    article = Article.new(nil, nil, author, nil)
     
     expect(article.author).to eq author
   end
 
   it 'stores any author passed to it' do
     author = ['Chemist']
-    article = Article.new(nil, nil, author)
+    article = Article.new(nil, nil, author, nil)
 
     expect(article.author).to eq author
   end
@@ -58,7 +59,7 @@ describe 'An article' do
   describe 'an article with multiple authors' do
     it 'stores all the authors' do
       authors = ['Author 1', 'Author 2', 'Author 3']
-      article = Article.new(nil, nil, authors)
+      article = Article.new(nil, nil, authors, nil)
       
       expect(article.author).to eq authors
     end
@@ -66,7 +67,14 @@ describe 'An article' do
 
   it 'stores its ISSN' do
     issn = '1234-5678'
-    article = Article.new(nil, nil, nil)
+    article = Article.new(nil, nil, nil, issn)
+    
+    expect(article.issn).to eq issn
+  end
+
+  it 'stores any ISSN passed to it' do
+    issn = '2345-6789'
+    article = Article.new(nil, nil, nil, issn)
     
     expect(article.issn).to eq issn
   end
