9f1415a6e5c9fcd4baea3e847920168def31f32f
Hemal N Varambhia <h.n.varambhia@gmail.com>
2016-03-18 18:22:47 +0000

    The constructor is slightly bloated so a hash has been introduced the avoid order or parameters dependency

--- spec/article_spec.rb
+++ spec/article_spec.rb
@@ -2,11 +2,11 @@ describe 'An article' do
   class Article
     attr_reader :doi, :title, :author
 
-    def initialize(doi, title, author, issn)
-      @doi = doi 
-      @title = title
-      @author = author
-      @issn = issn
+    def initialize(args)
+      @doi = args[:doi] 
+      @title = args[:title]
+      @author = args[:author]
+      @issn = args[:issn]
     end
 
     def issn
@@ -16,42 +16,42 @@ describe 'An article' do
 
   it 'stores its DOI' do
     doi = '10.1234/altmetric0'
-    article = Article.new(doi, nil, nil, nil)
+    article = Article.new(doi: doi)
     
     expect(article.doi).to eq doi
   end
 
   it 'stores any DOI passed to it' do
     doi = '10.1234/altmetric123'
-    article = Article.new(doi, nil, nil, nil)
+    article = Article.new(doi: doi)
 
     expect(article.doi).to eq doi
   end
 
   it 'stores the title' do
     title = 'Electron Scattering by HCN'
-    article = Article.new(nil, title, nil, nil)
+    article = Article.new(title: title)
 
     expect(article.title).to eq title
   end
 
   it 'stores any title passed to it' do
     title = 'Chemistry Article'
-    article = Article.new(nil, title, nil, nil)
+    article = Article.new(title: title)
 
     expect(article.title).to eq title
   end 
 
   it 'stores the author' do
     author = ['Physicist']
-    article = Article.new(nil, nil, author, nil)
+    article = Article.new(author: author)
     
     expect(article.author).to eq author
   end
 
   it 'stores any author passed to it' do
     author = ['Chemist']
-    article = Article.new(nil, nil, author, nil)
+    article = Article.new(author: author)
 
     expect(article.author).to eq author
   end
@@ -59,7 +59,7 @@ describe 'An article' do
   describe 'an article with multiple authors' do
     it 'stores all the authors' do
       authors = ['Author 1', 'Author 2', 'Author 3']
-      article = Article.new(nil, nil, authors, nil)
+      article = Article.new(author: authors)
       
       expect(article.author).to eq authors
     end
@@ -67,14 +67,14 @@ describe 'An article' do
 
   it 'stores its ISSN' do
     issn = '1234-5678'
-    article = Article.new(nil, nil, nil, issn)
+    article = Article.new(issn: issn)
     
     expect(article.issn).to eq issn
   end
 
   it 'stores any ISSN passed to it' do
     issn = '2345-6789'
-    article = Article.new(nil, nil, nil, issn)
+    article = Article.new(issn: issn)
     
     expect(article.issn).to eq issn
   end
