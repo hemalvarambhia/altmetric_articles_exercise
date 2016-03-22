40787fdf44d8968ac91184b256526f5b5c2cb5f6
Hemal <h.n.varambhia@gmail.com>
2016-03-22 12:36:45 +0000

    The Journal stores any ISSN assigned to it

--- spec/journal_spec.rb
+++ spec/journal_spec.rb
@@ -1,35 +1,34 @@
 describe 'A Journal' do
   class Journal
-    attr_reader :title
+    attr_reader :title, :issn
 
-    def initialize(title)
+    def initialize(title, issn)
       @title = title
-    end
-
-    def title
-      @title
-    end
-
-    def issn
-      '1234-5678'
+      @issn = issn
     end
   end
 
   it 'stores its name' do
-    journal = Journal.new('Journal of Physics B')
+    journal = Journal.new('Journal of Physics B', nil)
 
     expect(journal.title).to eq 'Journal of Physics B'
   end
 
   it 'stores any title passed to it' do
-    journal = Journal.new('Chemical Physics Letters')
+    journal = Journal.new('Chemical Physics Letters', nil)
 
     expect(journal.title).to eq 'Chemical Physics Letters'
   end
 
   it 'stores its ISSN' do
-    journal = Journal.new(nil)
+    journal = Journal.new(nil, '1234-5678')
 
     expect(journal.issn).to eq '1234-5678'
   end 
+
+  it 'stores any ISSN' do
+    journal = Journal.new(nil, '2468-1012')
+
+    expect(journal.issn).to eq '2468-1012'
+  end
 end
