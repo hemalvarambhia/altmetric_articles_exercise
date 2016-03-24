398543e8e246572e7cac8a1b58f346ffa4d54ec8
Hemal <h.n.varambhia@gmail.com>
2016-03-24 14:21:06 +0000

    Removed redundant tests

--- spec/journal_spec.rb
+++ spec/journal_spec.rb
@@ -12,30 +12,6 @@ describe 'A Journal' do
     end
   end
 
-  it 'stores its title' do
-    journal = Journal.new('Journal of Physics B', nil)
-
-    expect(journal.title).to eq 'Journal of Physics B'
-  end
-
-  it 'stores any title passed to it' do
-    journal = Journal.new('Chemical Physics Letters', nil)
-
-    expect(journal.title).to eq 'Chemical Physics Letters'
-  end
-
-  it 'stores its ISSN' do
-    journal = Journal.new(nil, '1234-5678')
-
-    expect(journal.issn).to eq '1234-5678'
-  end 
-
-  it 'stores any ISSN' do
-    journal = Journal.new(nil, '2468-1012')
-
-    expect(journal.issn).to eq '2468-1012'
-  end
-
   describe 'Equating two journals' do
     context 'when they have the same ISSN' do
      it 'marks them as being the same' do
