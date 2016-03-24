21c3521b9b06c7d495ec76d67ad3b0ea7a8d9bb7
Hemal <h.n.varambhia@gmail.com>
2016-03-24 16:36:12 +0000

    Replaced the test double with Article

--- spec/article_json_doc_spec.rb
+++ spec/article_json_doc_spec.rb
@@ -1,9 +1,12 @@
+$LOAD_PATH.unshift '../lib'
+require 'article'
+
 describe 'Article JSON Doc' do
   def render article
    {
       'doi' => article.doi, 
       'title' => article.title, 
-      'author' => article.author.join(', '),
+      'author' => (article.author || []).join(', '),
       'issn' => article.issn,
       'journal' => article.journal
    }
@@ -11,33 +14,29 @@ describe 'Article JSON Doc' do
 
   it 'publishes the DOI' do
     doi = '10.1234/altmetric0'
-    article = double(:article).as_null_object
+    article = Article.new(doi: doi)
 
-    expect(article).to receive(:doi).and_return doi
     expect(render(article)['doi']).to eq doi
   end
 
   it 'publishes the title' do
     title = 'Physics article'
-    article = double(:article).as_null_object
+    article = Article.new(title: title)
 
-    expect(article).to receive(:title).and_return title
     expect(render(article)['title']).to eq title
   end
 
   it 'publishes the author' do
     author = ['Physicist']
-    article = double(:author).as_null_object
+    article = Article.new(author: author)
 
-    expect(article).to receive(:author).and_return author
     expect(render(article)['author']).to eq 'Physicist'
   end
 
   describe 'an article with multiple authors' do
     it 'publishes all the authors comma separated' do
       authors = ['Physicist 1', 'Physicist 2', 'Physicist 3']
-      article = double(:article).as_null_object
-      allow(article).to receive(:author).and_return authors
+      article = Article.new(author: authors)
 
       expect(render(article)['author']).
         to eq 'Physicist 1, Physicist 2, Physicist 3'
@@ -46,17 +45,15 @@ describe 'Article JSON Doc' do
 
   it 'publishes the ISSN' do
     issn = '1234-5678'
-    article = double(:article).as_null_object
+    article = Article.new(issn: issn)
 
-    expect(article).to receive(:issn).and_return issn  
     expect(render(article)['issn']).to eq issn
   end
 
   it 'publishes the journal' do
     journal = 'Journal of Physics B'
-    article = double(:article).as_null_object
+    article = Article.new(journal: journal)
 
-    expect(article).to receive(:journal).and_return journal
     expect(render(article)['journal']).to eq journal
   end
 end
