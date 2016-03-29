6d9813dad57e594a42717e97eec8b5a04c2d77d3
Hemal <h.n.varambhia@gmail.com>
2016-03-29 14:27:24 +0100

    An ISSN with 8 numbers and a dash in the middle is a well formed one

--- spec/issn_spec.rb
+++ spec/issn_spec.rb
@@ -1,14 +1,28 @@
 describe 'ISSNs' do
   class ISSN
+    attr_reader :code
     Malformed = Class.new(Exception)
     
     def initialize(code)
-      raise Malformed.new
+      digits = code.scan(/\d/)
+      raise Malformed.new unless digits.count == 8
+      @code = code
     end
   end
+
   context 'when blank' do
     it 'is malformed' do
       expect{ ISSN.new('') }.to raise_error ISSN::Malformed 
+    end
+  end
+
+  context "when it consists of just 8 numbers and a '-' in the middle" do
+    it 'is not malformed' do
+      expect{ ISSN.new('1234-5678') }.not_to raise_error
+    end
+
+    it 'stores the code' do
+      expect(ISSN.new('1234-5678').code).to eq '1234-5678'
     end
   end
 end
