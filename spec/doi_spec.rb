6fa2065240d98f2d35b8ff8a0740eeb84a873ee5
Hemal <h.n.varambhia@gmail.com>
2016-04-04 13:41:50 +0100

    Typo corrected

--- spec/doi_spec.rb
+++ spec/doi_spec.rb
@@ -23,7 +23,7 @@ describe 'DOIs' do
   end
 
   context 'when it has an invalid registry' do
-    it 'is well-formed' do
+    it 'is malformed' do
       expect { DOI.new '12.'}.to raise_error DOI::Malformed
     end
   end
