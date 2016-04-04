17ec5d0beae57c327d615b53ad6b644a12a0a26d
Hemal <h.n.varambhia@gmail.com>
2016-04-04 13:38:19 +0100

    A DOI with a valid registry (10.) is well formed

--- spec/doi_spec.rb
+++ spec/doi_spec.rb
@@ -4,7 +4,8 @@ describe 'DOIs' do
     Malformed = Class.new(Exception)
 
     def initialize(doi)
-      raise Malformed.new
+      raise Malformed.new if doi.nil? or doi.length == 0
+      raise Malformed.new unless doi[0..2] == '10.'
     end
   end
 
@@ -12,6 +13,18 @@ describe 'DOIs' do
     it 'is malformed' do
       expect { DOI.new('') }.to raise_error DOI::Malformed
       expect { DOI.new nil }.to raise_error DOI::Malformed
+    end
+  end
+
+  context 'when it has the correct registry' do
+    it 'is well-formed' do
+      expect { DOI.new '10.' }.not_to raise_error DOI::Malformed
+    end
+  end
+
+  context 'when it has an invalid registry' do
+    it 'is well-formed' do
+      expect { DOI.new '12.'}.to raise_error DOI::Malformed
     end
   end
 end
