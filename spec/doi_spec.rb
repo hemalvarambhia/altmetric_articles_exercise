eadfeae7454ac7278ba5348a7e5a2c00ecb29cc4
Hemal <h.n.varambhia@gmail.com>
2016-04-04 13:28:44 +0100

    Developing specs for the DOI of an article - a blank one is malformed

---
+++ spec/doi_spec.rb
@@ -0,0 +1,17 @@
+describe 'DOIs' do
+  DOI = Class.new
+  class DOI
+    Malformed = Class.new(Exception)
+
+    def initialize(doi)
+      raise Malformed.new
+    end
+  end
+
+  context 'when the DOI is blank' do
+    it 'is malformed' do
+      expect { DOI.new('') }.to raise_error DOI::Malformed
+      expect { DOI.new nil }.to raise_error DOI::Malformed
+    end
+  end
+end
