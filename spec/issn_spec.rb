1d4375fe034964851738c7a86166a55ae3243b31
Hemal <h.n.varambhia@gmail.com>
2016-04-04 12:35:01 +0100

    ISSNs with the same code are equal

--- spec/issn_spec.rb
+++ spec/issn_spec.rb
@@ -8,6 +8,10 @@ describe 'ISSNs' do
       @code = code
       @code = code.insert(4, '-') if code.scan(/-/).none?
     end
+
+    def ==(other)
+      code == other.code
+    end
   end
 
   context 'when blank' do
@@ -46,6 +50,26 @@ describe 'ISSNs' do
   context "when it contains any non-word character other than '-'" do
     it 'is malformed' do
       expect{ ISSN.new('5643-/1111') }.to raise_error ISSN::Malformed
+    end
+  end
+
+  describe '#equals' do
+    context 'when the ISSNs are different' do
+      it 'marks them as being unequal' do
+        issn = ISSN.new '4325-8760'
+        different_issn = ISSN.new '3289-4444'
+
+        expect(issn).not_to eq different_issn
+      end
+    end
+
+    context 'when the ISSN codes are the same' do
+      it 'marks them as being the same' do
+        issn = ISSN.new '5190-7189'
+        with_same_code = ISSN.new '5190-7189'
+
+        expect(issn).to eq with_same_code
+      end
     end
   end
 end
