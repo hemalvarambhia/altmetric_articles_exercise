cc724942de2c4c00d29cc2771540d20d13ec2de8
Hemal <h.n.varambhia@gmail.com>
2016-04-04 16:16:01 +0100

    Error message is more readable

--- spec/issn_spec.rb
+++ spec/issn_spec.rb
@@ -1,10 +1,18 @@
 describe 'ISSNs' do
   class ISSN
     attr_reader :code
-    Malformed = Class.new(Exception)
+
+    class Malformed < Exception
+      def initialize(code)
+        message = "The ISSN '#{code}' is malformed. "
+        message << "ISSNs take the form \d{4}-\d{4} e.g. '0317-8471'"
+        super message
+      end
+    end
     
     def initialize(code)
-      raise Malformed.new if (code=~/^\d{4}-?\d{4}$/).nil?      
+      raise Malformed.new(code) if (code=~/^\d{4}-?\d{4}$/).nil?      
+
       @code = code
       @code = code.insert(4, '-') if code.scan(/-/).none?
     end
