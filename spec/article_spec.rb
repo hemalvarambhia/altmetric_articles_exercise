0e57767a132f79ac8195664d2a23639fbb7454b1
Hemal <h.n.varambhia@gmail.com>
2016-04-11 14:28:40 +0100

    We now pass in the DOI object to the Article

--- spec/article_spec.rb
+++ spec/article_spec.rb
@@ -1,5 +1,6 @@
 require 'issn'
 require 'journal'
+require 'doi'
 require 'article'
 
 describe 'An article' do
@@ -15,9 +16,9 @@ describe 'An article' do
   describe '#equals' do
     context 'when they both have the same DOI' do
       it 'marks them as being the same' do
-        article = Article.new(doi: '10.1234/altmetric123')
+        article = Article.new(doi: DOI.new('10.1234/altmetric123'))
         article_with_same_doi = Article.new(
-          doi: '10.1234/altmetric123'
+          doi: DOI.new('10.1234/altmetric123')
         )
         
         expect(article).to eq article_with_same_doi
@@ -26,31 +27,31 @@ describe 'An article' do
 
     context 'when they have different DOIs' do
       it 'marks them as not being the same' do
-        article = Article.new(doi: '10.1234/altmetric222')
+        article = Article.new(doi: DOI.new('10.1234/altmetric222'))
         with_different_doi = Article.new(
-          doi: '10.1234/altmetric999'
+          doi: DOI.new('10.1234/altmetric999')
         )
         expect(article).not_to eq with_different_doi
       end
     end
 
     it 'is reflexive' do
-      article = Article.new(doi: '10.1234/altmetric345')
+      article = Article.new(doi: DOI.new('10.1234/altmetric345'))
       
       expect(article).to eq article
     end
 
     it 'is symmetric' do
-      article_1 = Article.new(doi: '10.1234/altmetric123')
-      article_2 = Article.new(doi: '10.1234/altmetric123')
+      article_1 = Article.new(doi: DOI.new('10.1234/altmetric123'))
+      article_2 = Article.new(doi: DOI.new('10.1234/altmetric123'))
       
       expect(article_1).to eq article_2
     end
 
     it 'is transitive' do
-      article_1 = Article.new(doi: '10.1234/altmetric541')
-      article_2 = Article.new(doi: '10.1234/altmetric541')
-      article_3 = Article.new(doi: '10.1234/altmetric541')
+      article_1 = Article.new(doi: DOI.new('10.1234/altmetric541'))
+      article_2 = Article.new(doi: DOI.new('10.1234/altmetric541'))
+      article_3 = Article.new(doi: DOI.new('10.1234/altmetric541'))
       
       expect(article_1).to eq article_2
       expect(article_2).to eq article_3
