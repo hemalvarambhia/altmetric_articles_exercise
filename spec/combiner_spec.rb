34c867fbf74b67ccf61963059cd972f93bed461d
Hemal N Varambhia <h.n.varambhia@gmail.com>
2016-04-12 13:46:21 +0100

    No need for the context

--- spec/combiner_spec.rb
+++ spec/combiner_spec.rb
@@ -22,42 +22,40 @@ describe 'Combiner' do
     end
   end
   
-  context 'when there is a journal and authors(s)' do
-    before(:each) do
-      @journals_file = double(:journals_file)
-      @authors_file = double(:authors_file)
-    end
+  before(:each) do
+    @journals_file = double(:journals_file)
+    @authors_file = double(:authors_file)
+  end
     
-    it 'merges them together with the article' do
-      articles_file = double(:articles_file)
-      expect(articles_file)
-        .to(receive(:each)
-             .and_yield(                     
-                 {
-                   doi: DOI.new('10.5649/altmetric098'),
-                   title: 'Physics',
-                   issn: ISSN.new('1432-0456')
-                 }
-             )
+  it 'merges articles, journals and authors together' do
+    articles_file = double(:articles_file)
+    expect(articles_file)
+      .to(receive(:each)
+           .and_yield(                     
+             {
+               doi: DOI.new('10.5649/altmetric098'),
+               title: 'Physics',
+               issn: ISSN.new('1432-0456')
+             }
            )
-      allow(@journals_file).to(
-        receive(:find).with(ISSN.new('1432-0456'))
-        .and_return(Journal.new('Journal', ISSN.new('1432-0456'))))
-      allow(@authors_file).to(
-        receive(:find).with(DOI.new('10.5649/altmetric098'))
-        .and_return(['Author 1', 'Author 2', 'Author 3']))
-      combiner = Combiner.new(
-        articles_file, @journals_file, @authors_file)
+         )
+    allow(@journals_file).to(
+      receive(:find).with(ISSN.new('1432-0456'))
+      .and_return(Journal.new('Journal', ISSN.new('1432-0456'))))
+    allow(@authors_file).to(
+      receive(:find).with(DOI.new('10.5649/altmetric098'))
+      .and_return(['Author 1', 'Author 2', 'Author 3']))
+    combiner = Combiner.new(
+      articles_file, @journals_file, @authors_file)
       
-      expect { |b| combiner.each(&b) }.to(
-        yield_successive_args(
-          {
-            doi: DOI.new('10.5649/altmetric098'),
-            title: 'Physics',
-            journal: Journal.new('Journal', ISSN.new('1432-0456')),
-            authors: [ 'Author 1', 'Author 2', 'Author 3' ]
-          }
-        ))
-    end
+    expect { |b| combiner.each(&b) }.to(
+      yield_successive_args(
+        {
+          doi: DOI.new('10.5649/altmetric098'),
+          title: 'Physics',
+          journal: Journal.new('Journal', ISSN.new('1432-0456')),
+          authors: [ 'Author 1', 'Author 2', 'Author 3' ]
+        }
+      ))
   end
 end
