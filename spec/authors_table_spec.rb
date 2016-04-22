2192ecb3043b103f6d36b65f2a91e5bdfe998629
Hemal <h.n.varambhia@gmail.com>
2016-04-22 13:00:20 +0100

    When the table does not have the author of a publication, it returns no authors

--- spec/authors_table_spec.rb
+++ spec/authors_table_spec.rb
@@ -1,16 +1,34 @@
+require 'ostruct'
 require 'doi'
+
 describe 'Authors table' do
   class AuthorsTable
+    def initialize(authors = [])
+
+    end
+
     def find(doi)
       []
     end
   end
 
   context 'When the table is empty' do
-    it 'return no authors' do
+    it 'returns no authors' do
       authors_table = AuthorsTable.new
 
       expect(authors_table.find(DOI.new('10.6453/altmetric490'))).to eq []
     end 
   end
+
+  context 'When there are no authors of an article' do
+    it 'returns no authors' do
+      authors_table = AuthorsTable.new(
+        [
+          OpenStruct.new(name: 'Author 1', publications: [DOI.new('10.3454/altmetric001')])
+        ]
+      )
+
+      expect(authors_table.find(DOI.new('10.8899/altmetric02324'))).to eq []
+    end
+  end 
 end
