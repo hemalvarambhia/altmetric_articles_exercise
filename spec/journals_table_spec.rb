0283d31b66ba3fc71ab2addc1ac4e94427947c12
Hemal <h.n.varambhia@gmail.com>
2016-04-22 11:03:28 +0100

    When there is no journal with the specified ISSN, no title is returned

--- spec/journals_table_spec.rb
+++ spec/journals_table_spec.rb
@@ -1,6 +1,10 @@
 require 'issn'
 describe 'Journals table' do
   class JournalsTable
+    def initialize(rows = [])
+
+    end
+
     def find(issn)
       ''
     end
@@ -12,6 +16,19 @@ describe 'Journals table' do
         journals_table = JournalsTable.new
         
         expect(journals_table.find(ISSN.new('1234-4384'))).to eq ''
+      end
+    end
+
+    context 'When the table does not have a journal with the ISSN' do
+      it 'returns no title' do
+        journals_table = JournalsTable.new(
+          [ 
+            [ISSN.new('5155-3789'), 'Journal of Physics A'],
+            [ISSN.new('8654-5652'), 'Journal of Physics B']
+          ]
+        )
+
+        expect(journals_table.find(ISSN.new('8766-8334'))).to eq ''
       end
     end
   end
