85936d9fe2d94ba021945069d4909358ea64560a
Hemal <h.n.varambhia@gmail.com>
2016-04-22 14:12:09 +0100

    We spec-ing out the find method

--- spec/authors_table_spec.rb
+++ spec/authors_table_spec.rb
@@ -13,37 +13,39 @@ describe 'Authors table' do
       [author.name]
     end
   end
+  
+  describe '#find' do
+    context 'When the table is empty' do
+      it 'returns no authors' do
+        authors_table = AuthorsTable.new
 
-  context 'When the table is empty' do
-    it 'returns no authors' do
-      authors_table = AuthorsTable.new
-
-      expect(authors_table.find(DOI.new('10.6453/altmetric490'))).to eq []
-    end 
-  end
-
-  context 'When there are no authors of an article' do
-    it 'returns no authors' do
-      authors_table = AuthorsTable.new(
-        [
-          OpenStruct.new(name: 'No Match 1', publications: [a_doi]),
-          OpenStruct.new(name: 'No Match 2', publications: [a_doi])
-        ]
-      )
-
-      expect(authors_table.find(DOI.new('10.8899/altmetric02324'))).to eq []
+        expect(authors_table.find(DOI.new('10.6453/altmetric490'))).to eq []
+      end 
     end
-  end 
 
-  context 'When the table contains the author of an article' do
-    it 'returns the name of the author' do
-      authors_table = AuthorsTable.new(
-        [
-          OpenStruct.new(name: 'Author', publications: [ DOI.new('10.1111/altmetric777') ]),
-          OpenStruct.new(name: 'No Matching', publications: [a_doi])       
-        ]
-      )
-      expect(authors_table.find(DOI.new('10.1111/altmetric777'))).to eq ['Author']
+    context 'When there are no authors of an article' do
+      it 'returns no authors' do
+        authors_table = AuthorsTable.new(
+          [
+            OpenStruct.new(name: 'No Match 1', publications: [a_doi]),
+            OpenStruct.new(name: 'No Match 2', publications: [a_doi])
+          ]
+        )
+
+        expect(authors_table.find(DOI.new('10.8899/altmetric02324'))).to eq []
+      end
+    end 
+
+    context 'When the table contains the author of an article' do
+      it 'returns the name of the author' do
+        authors_table = AuthorsTable.new(
+          [
+            OpenStruct.new(name: 'Author', publications: [ DOI.new('10.1111/altmetric777') ]),
+            OpenStruct.new(name: 'No Matching', publications: [a_doi])       
+          ]
+        )
+        expect(authors_table.find(DOI.new('10.1111/altmetric777'))).to eq ['Author']
+      end
     end
   end
 
