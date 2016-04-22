954278df1761d61827a2a38d0d9bcd23eb97f12b
Hemal <h.n.varambhia@gmail.com>
2016-04-22 11:23:07 +0100

    When the table does contain the journal, it returns the title

--- spec/journals_table_spec.rb
+++ spec/journals_table_spec.rb
@@ -2,11 +2,11 @@ require 'issn'
 describe 'Journals table' do
   class JournalsTable
     def initialize(rows = [])
-
+      @journals = Hash[rows]
     end
 
     def find(issn)
-      ''
+      @journals[issn] || ''
     end
   end
 
@@ -29,6 +29,21 @@ describe 'Journals table' do
         )
 
         expect(journals_table.find(ISSN.new('8766-8334'))).to eq ''
+      end
+    end
+
+    context 'When the table has a journal with the given ISSN' do
+      it 'returns the title' do
+        journals_table = JournalsTable.new(
+          [
+            [ISSN.new('2976-9674'), 'Chemical Physics Letters'],
+            [ISSN.new('6195-6091'), 'Nature']
+          ]
+        )
+
+        expect(journals_table.find(ISSN.new('2976-9674'))).to(
+          eq 'Chemical Physics Letters'
+        )
       end
     end
   end
