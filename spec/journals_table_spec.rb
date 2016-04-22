4b6c59b6004fb621e60b80a348a44382223beab7
Hemal <h.n.varambhia@gmail.com>
2016-04-22 11:54:10 +0100

    Example not needed anymore

--- spec/journals_table_spec.rb
+++ spec/journals_table_spec.rb
@@ -11,14 +11,6 @@ describe 'Journals table' do
   end
 
   describe '#find' do
-    context 'When the table is empty' do
-      it 'returns no title' do
-        journals_table = JournalsTable.new
-        
-        expect(journals_table.find(ISSN.new('1234-4384'))).to eq ''
-      end
-    end
-
     context 'When the table does not have a journal with the ISSN' do
       it 'returns no title' do
         journals_table = JournalsTable.new(
