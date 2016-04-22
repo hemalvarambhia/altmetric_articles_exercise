a7ba42ebcc3477b5d7a6e52e2ca95ce7c26276dd
Hemal <h.n.varambhia@gmail.com>
2016-04-22 13:14:38 +0100

    When there is an author of a given publication, find returns their name. For clarity of purpose, we use a doi creation helper method in the other examples

--- spec/authors_table_spec.rb
+++ spec/authors_table_spec.rb
@@ -4,11 +4,13 @@ require 'doi'
 describe 'Authors table' do
   class AuthorsTable
     def initialize(authors = [])
-
+      @authors = authors
     end
 
     def find(doi)
-      []
+      author = @authors.detect { |author| author.publications.include?(doi) }
+      return [] if author.nil?
+      [author.name]
     end
   end
 
@@ -24,12 +26,28 @@ describe 'Authors table' do
     it 'returns no authors' do
       authors_table = AuthorsTable.new(
         [
-          OpenStruct.new(name: 'Author 1', publications: [DOI.new('10.3454/altmetric001')]),
-          OpenStruct.new(name: 'Author 1', publications: [DOI.new('10.4854/altmetric021')])
+          OpenStruct.new(name: 'Author 1', publications: [a_doi]),
+          OpenStruct.new(name: 'Author 2', publications: [a_doi])
         ]
       )
 
       expect(authors_table.find(DOI.new('10.8899/altmetric02324'))).to eq []
     end
   end 
+
+  context 'When the table contains the author of an article' do
+    it 'returns the name of the author' do
+      authors_table = AuthorsTable.new(
+        [
+          OpenStruct.new(name: 'Author', publications: [ DOI.new('10.1111/altmetric777') ]),
+          OpenStruct.new(name: 'Non-matching', publications: [a_doi])       
+        ]
+      )
+      expect(authors_table.find(DOI.new('10.1111/altmetric777'))).to eq ['Author']
+    end
+  end
+
+  def a_doi
+    DOI.new("10.6965/altmetric#{rand(100000000)}")
+  end
 end
