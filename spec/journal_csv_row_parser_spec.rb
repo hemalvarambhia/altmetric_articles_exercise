3f248b3cb5d869c4e29803dd23d745f0a7e5f9bb
Hemal <h.n.varambhia@gmail.com>
2016-04-25 16:43:40 +0100

    Added spacing for clarity

--- spec/journal_csv_row_parser_spec.rb
+++ spec/journal_csv_row_parser_spec.rb
@@ -5,6 +5,7 @@ describe 'Parsing a line from the journal CSV file' do
       [ISSN.new('1975-3545'), 'Journal of Mathematics']
     end
   end
+
   it 'reads off the ISSN and title' do
     line = ['1975-3545', 'Journal of Mathematics']
     expect(JournalCSVParser.parse(line)).to(
