a5fb2b9662fc2b20904c41aa634570a7ab9fcad1
Hemal <h.n.varambhia@gmail.com>
2016-04-25 12:33:18 +0100

    Spec for the JournalCSVFile

---
+++ spec/journal_csv_file_spec.rb
@@ -0,0 +1,27 @@
+require 'csv'
+describe 'Journal CSV File' do
+  class JournalCSVFile
+    def initialize(io, parser)
+      @io = io
+      @parser = parser
+    end
+
+    def read
+      CSV.new(@io).each { |line| @parser.parse(line) }
+
+    end
+  end
+
+  it 'parses off the ISSN and title' do
+    io = StringIO.open do |content|
+      content << '8964-5695,Journal of Physics B'
+      content.string
+    end
+    line = [ '8964-5695', 'Journal of Physics B' ]
+    parser = double(:parser)
+    expect(parser).to receive(:parse).with(line)
+    file = JournalCSVFile.new(io, parser)
+
+    file.read
+  end
+end
