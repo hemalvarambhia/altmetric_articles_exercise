5b64029f016759c9ac8b371a63b9925a8c10d02e
Hemal <h.n.varambhia@gmail.com>
2016-04-26 13:30:39 +0100

    Parser should return a hash

--- spec/article_csv_row_parser_spec.rb
+++ spec/article_csv_row_parser_spec.rb
@@ -3,13 +3,22 @@ require 'issn'
 describe 'Article CSV Parser' do
   class ArticleCSVParser
     def self.parse(line)
-      [DOI.new('10.4937/altmetric001'), 'Physics', ISSN.new('1232-1983')]
+      {
+        doi: DOI.new('10.4937/altmetric001'), 
+        title: 'Physics', 
+        issn: ISSN.new('1232-1983')
+      }
     end
   end
   
-  it 'reads off the DOI, title and ISSN' do
+  it 'reads off the DOI, title and ISSN from a line' do
     line = [ '10.4937/altmetric001', 'Physics', '1232-1983' ]
-    expect(ArticleCSVParser.parse(line)).to(
-      eq([DOI.new('10.4937/altmetric001'), 'Physics', ISSN.new('1232-1983')]))
+    expected = { 
+      doi: DOI.new('10.4937/altmetric001'),
+      title: 'Physics',
+      issn: ISSN.new('1232-1983')
+    }
+
+    expect(ArticleCSVParser.parse(line)).to eq expected
   end
 end
