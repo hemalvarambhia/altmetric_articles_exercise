4011c8704c05b95080fa44e1a8ca836a6a8ccf07
Hemal <h.n.varambhia@gmail.com>
2016-04-26 13:20:20 +0100

    Triangulated with an additional test - we can parse any line from a CSV file

--- spec/journal_csv_row_parser_spec.rb
+++ spec/journal_csv_row_parser_spec.rb
@@ -2,13 +2,22 @@ require 'issn'
 describe 'Parsing a line from the journal CSV file' do
   class JournalCSVParser
     def self.parse line
-      [ISSN.new('1975-3545'), 'Journal of Mathematics']
+      [ISSN.new(line.first), line.last]
     end
   end
 
   it 'reads off the ISSN and title' do
     line = ['1975-3545', 'Journal of Mathematics']
+
     expect(JournalCSVParser.parse(line)).to(
         eq([ISSN.new('1975-3545'), 'Journal of Mathematics']))
+  end
+
+  it 'reads off any ISSN and title' do
+    line = ['2016-1623','Astrophysical Journal']
+
+    expect(JournalCSVParser.parse(line)).to(
+      eq([ISSN.new('2016-1623'), 'Astrophysical Journal'])
+    )
   end
 end
