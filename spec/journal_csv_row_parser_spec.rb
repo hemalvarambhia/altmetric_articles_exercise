cfedbbd8283d648f6a0d947c6b0dc22beba879ba
Hemal <h.n.varambhia@gmail.com>
2016-04-26 13:38:01 +0100

    Improved the description of the behaviour

--- spec/journal_csv_row_parser_spec.rb
+++ spec/journal_csv_row_parser_spec.rb
@@ -6,14 +6,14 @@ describe 'Parsing a line from the journal CSV file' do
     end
   end
 
-  it 'reads off the ISSN and title' do
+  it 'reads off the ISSN and title from a line' do
     line = ['1975-3545', 'Journal of Mathematics']
 
     expect(JournalCSVParser.parse(line)).to(
         eq([ISSN.new('1975-3545'), 'Journal of Mathematics']))
   end
 
-  it 'reads off any ISSN and title' do
+  it 'reads off the ISSN and title from any line' do
     line = ['2016-1623','Astrophysical Journal']
 
     expect(JournalCSVParser.parse(line)).to(
