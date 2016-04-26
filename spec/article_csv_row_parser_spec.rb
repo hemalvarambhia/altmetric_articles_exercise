9c80aa8079c1f276838139c0cab061799ef6c0f5
Hemal <h.n.varambhia@gmail.com>
2016-04-26 13:35:27 +0100

    Triangulated - no we can read off the DOI, title and ISSN from any line

--- spec/article_csv_row_parser_spec.rb
+++ spec/article_csv_row_parser_spec.rb
@@ -4,9 +4,9 @@ describe 'Article CSV Parser' do
   class ArticleCSVParser
     def self.parse(line)
       {
-        doi: DOI.new('10.4937/altmetric001'), 
-        title: 'Physics', 
-        issn: ISSN.new('1232-1983')
+        doi: DOI.new(line.first), 
+        title: line[1], 
+        issn: ISSN.new(line.last)
       }
     end
   end
@@ -17,6 +17,17 @@ describe 'Article CSV Parser' do
       doi: DOI.new('10.4937/altmetric001'),
       title: 'Physics',
       issn: ISSN.new('1232-1983')
+    }
+
+    expect(ArticleCSVParser.parse(line)).to eq expected
+  end
+
+  it 'reads off the DOI, title and ISSN from any line' do
+    line = [ '10.7503/altmetric123', 'Astrophysics', '1912-2223' ]
+    expected = {
+      doi: DOI.new('10.7503/altmetric123'),
+      title: 'Astrophysics',
+      issn: ISSN.new('1912-2223')
     }
 
     expect(ArticleCSVParser.parse(line)).to eq expected
