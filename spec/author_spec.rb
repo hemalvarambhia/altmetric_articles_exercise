cafaffe8aa8088100c9694296180ac3be7bfbe6a
Hemal <h.n.varambhia@gmail.com>
2016-04-29 12:44:40 +0100

    Replaced hard-coded DOIs with ones created by a helper method

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -8,28 +8,30 @@ describe 'Author' do
   end
 
   it 'stores their publications' do
-    doi_1 = DOI.new('10.1234/altmetric2394')
-    doi_2 = DOI.new('10.7808/altmetric6333')
+    doi_1, doi_2 = a_doi, a_doi
     author = ArticleAuthor.new(name: nil, publications: [doi_1, doi_2])
+
     expect(author.publications).to eq [doi_1, doi_2]
   end
 
   describe '#author_of?' do
     it 'confirms that an author published an article' do
-      doi_1 = DOI.new('10.0011/altmetric9999')
-      doi_2 = DOI.new('10.7688/altmetric6512')
+      doi_1, doi_2 = a_doi, a_doi
       author = ArticleAuthor.new(name: nil, publications: [doi_1, doi_2])
 
       expect(author).to be_author_of doi_1
     end
 
     it 'confirms that an author did not publish an article' do
-      doi_1 = DOI.new('10.0011/altmetric9999')
-      doi_2 = DOI.new('10.7688/altmetric6512')
-      doi_3 = DOI.new('10.2211/altmetric1983')
+      doi_1, doi_2, doi_3 = a_doi, a_doi, a_doi
       author = ArticleAuthor.new(name: nil, publications: [doi_1, doi_2])
 
       expect(author).not_to be_author_of doi_3
     end
+  end
+
+  def a_doi
+    registrant = Array.new(4) { rand(0..9) }.join
+    DOI.new("10.#{registrant}/altmetric#{rand(100000000)}")
   end
 end
