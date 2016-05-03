181b751cdf21a5ba9b749936974786ed1d40bc3b
Hemal <h.n.varambhia@gmail.com>
2016-05-03 11:33:29 +0100

    We can now add JSON elements to the document. The test double is a supporting part of the example

--- spec/json_document_spec.rb
+++ spec/json_document_spec.rb
@@ -5,8 +5,12 @@ describe 'JSON document' do
   include CreateDOI, CreateISSN
 
   class JSONDocument
-    def initialize(article = [])
-      @content = article
+    def initialize(articles = [])
+      @content = articles
+    end
+
+    def <<(object)
+      @content << object
     end
 
     def content
@@ -26,17 +30,29 @@ describe 'JSON document' do
 
   describe '#content' do
     it "holds the document's content" do
-      expected = {
-        'doi' => a_doi,
+      expected = { 
+        'doi' => a_doi, 
         'title' => 'Physics Article',
         'issn' => an_issn
       }
       article = double(:article)
-      expect(article).to receive(:to_json).and_return expected
+      allow(article).to receive(:to_json).and_return expected
       json_doc = JSONDocument.new [ article ]
       
       expect(json_doc.content).to eq [ expected ]
       expect(json_doc).not_to be_empty
+    end
+  end
+
+  describe '#<<' do
+    it 'adds an article' do
+      json_doc = JSONDocument.new
+      expected = { 'doi' => a_doi, 'title' => 'Chemistry', 'issn' => an_issn }
+      article = double(:article)
+      allow(article).to receive(:to_json).and_return expected
+      json_doc << article
+  
+      expect(json_doc.content).to include expected
     end
   end
 end
