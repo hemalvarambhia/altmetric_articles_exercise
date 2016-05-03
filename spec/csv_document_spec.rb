d9d5910e9295843249c867a7625e094b21c6f06f
Hemal <h.n.varambhia@gmail.com>
2016-05-03 14:15:48 +0100

    We can add csv objects to the document in insertion order, elimininated arrange duplication

--- spec/csv_document_spec.rb
+++ spec/csv_document_spec.rb
@@ -8,7 +8,7 @@ describe 'CSV Document' do
     end
 
     def <<(object)
-      @content = [ object ]
+      @content << object
     end
 
     def content
@@ -38,14 +38,28 @@ describe 'CSV Document' do
   end
 
   describe '#<<' do
+    before(:each) { @doc = CSVDocument.new }
     it 'appends to the current content' do
       expected = [ a_doi, 'Quantum Mechanics', an_issn ]
-      csv_doc = CSVDocument.new
       article = double(:article, as_csv: expected)
 
-      csv_doc << article
+      @doc << article
 
-      expect(csv_doc.content).to eq [ expected ]
+      expect(@doc.content).to eq [ expected ]
+    end
+
+    it 'appends in insertion order' do
+      first = [ a_doi, 'Article 1', an_issn ]
+      second = [ a_doi, 'Article 2', an_issn ]
+      last = [ a_doi, 'Article 3', an_issn ]
+      [first, second, last].each do |expected_row|
+         article = double(:article, as_csv: expected_row)
+         @doc << article
+      end
+
+      expect(@doc.content[0]).to eq first
+      expect(@doc.content[1]).to eq second
+      expect(@doc.content[2]).to eq last
     end
   end
 end
