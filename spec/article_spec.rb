aa8ec78cb3efd1e834152da13b137e799cc2d45b
Hemal <h.n.varambhia@gmail.com>
2016-05-04 16:38:19 +0100

    Created a matcher to check objects are in their expected places in the CSV row. CSV representation renders the DOI

--- spec/article_spec.rb
+++ spec/article_spec.rb
@@ -21,6 +21,10 @@ describe 'Article' do
         'issn' => @issn
       }
     end
+
+    def as_csv
+      [ @doi ]
+    end
   end
  
   describe '#as_json' do
@@ -68,6 +72,39 @@ describe 'Article' do
       article_json = article.as_json
 
       expect(article_json).to include('issn' => expected)
+    end
+  end
+
+  describe '#as_csv' do
+    it 'renders the DOI' do
+      expected = a_doi
+      article = Article.new(doi: expected)
+
+      article_csv = article.as_csv
+
+      expect(article_csv).to have(expected).at_position(1)
+    end
+ 
+    it 'renders the title'
+
+    it 'renders the authors'
+
+    it 'renders the title of the journal'
+
+    it "renders the journal's ISSN"
+
+    RSpec::Matchers.define :have do |expected|
+      match do |actual|
+        actual.include?(expected) and actual.index(expected) == @expected_position
+      end
+
+      chain :at_position do |position|
+        @expected_position = position - 1
+      end
+
+      failure_message_for_should do |actual|
+       "expected that #{actual} would contain of #{expected} at position #{@expected_position}≈ß"
+      end
     end
   end
 end
