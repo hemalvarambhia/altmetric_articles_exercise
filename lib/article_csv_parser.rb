27a5d2b1d4b383d3db9306f15337613bb9ecb268
Hemal N Varambhia <h.n.varambhia@gmail.com>
2016-05-06 11:28:45 +0100

    Use the look up bracket to make code more generic

--- lib/article_csv_parser.rb
+++ lib/article_csv_parser.rb
@@ -3,9 +3,9 @@ require 'issn'
 class ArticleCSVParser
   def self.parse(row)
     {
-      doi: DOI.new(row.first),
+      doi: DOI.new(row[0]),
       title: row[1],
-      issn: ISSN.new(row.last)
+      issn: ISSN.new(row[2])
     }
   end
 end
