fb9e689106235623505c1482558c44b66a0b08ec
Hemal N Varambhia <h.n.varambhia@gmail.com>
2016-05-06 23:00:18 +0100

    The ISSN should be that specified in the one_article.csv doc

--- spec/integration/parsing_journals_csv_doc_spec.rb
+++ spec/integration/parsing_journals_csv_doc_spec.rb
@@ -1,12 +1,10 @@
-#Title,ISSN
-#Bartell-Collins,1167-8230
 require 'issn'
 require 'journal_csv_doc'
 require 'altmetric_file'
 require 'journal_csv_parser'
 describe 'Parsing Journal CSV Docs' do
   it 'reads of the title and ISSN' do
-    expected = [ ISSN.new('1167-8230'), 'Bartell-Collins' ]
+    expected = [ ISSN.new('1337-8688'), 'Bartell-Collins' ]
     path_to_journal_csv = File.join(
       File.dirname(__FILE__), 'sample_docs', 'one_journal.csv'
     )
