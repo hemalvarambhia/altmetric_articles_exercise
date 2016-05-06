20906ef8bdbde41f43b90ca30ebcc5dd4905d87d
Hemal N Varambhia <h.n.varambhia@gmail.com>
2016-05-06 12:04:16 +0100

    The column fields were in the wrong order

--- spec/journal_csv_parser_spec.rb
+++ spec/journal_csv_parser_spec.rb
@@ -1,20 +1,15 @@
 require 'issn'
+require 'journal_csv_parser'
 describe 'Parsing a line from the journal CSV file' do
-  class JournalCSVParser
-    def self.parse(row)
-      [ISSN.new(row.first), row.last]
-    end
-  end
-
   it 'reads off the ISSN and title from a line' do
-    line = ['1975-3545', 'Journal of Mathematics']
+    line = ['Journal of Mathematics', '1975-3545']
 
     expect(JournalCSVParser.parse(line)).to(
         eq([ISSN.new('1975-3545'), 'Journal of Mathematics']))
   end
 
   it 'reads off the ISSN and title from any line' do
-    line = ['2016-1623','Astrophysical Journal']
+    line = ['Astrophysical Journal', '2016-1623']
 
     expect(JournalCSVParser.parse(line)).to(
       eq([ISSN.new('2016-1623'), 'Astrophysical Journal'])
