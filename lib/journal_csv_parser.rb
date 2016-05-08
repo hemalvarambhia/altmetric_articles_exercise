f99026b3de44584d46c160019bb6868aab25c686
Hemal N Varambhia <h.n.varambhia@gmail.com>
2016-05-08 20:45:13 +0100

    Use the splat operator to allow multiple arguments

--- lib/journal_csv_parser.rb
+++ lib/journal_csv_parser.rb
@@ -1,6 +1,6 @@
 require 'issn'
 class JournalCSVParser
   def self.parse(row)
-    {ISSN.new(row[1]) => row[0]}
+    [ ISSN.new(row[1]), row[0] ]
   end
 end
--- spec/journal_csv_parser_spec.rb
+++ spec/journal_csv_parser_spec.rb
@@ -5,14 +5,14 @@ describe 'Parsing a line from the journal CSV file' do
     line = ['Journal of Mathematics', '1975-3545']
 
     expect(JournalCSVParser.parse(line)).to(
-        eq({ISSN.new('1975-3545') => 'Journal of Mathematics'}))
+        eq([ISSN.new('1975-3545'), 'Journal of Mathematics']))
   end
 
   it 'reads off the ISSN and title from any line' do
     line = ['Astrophysical Journal', '2016-1623']
 
     expect(JournalCSVParser.parse(line)).to(
-      eq({ISSN.new('2016-1623') => 'Astrophysical Journal'})
+      eq([ISSN.new('2016-1623'), 'Astrophysical Journal'])
     )
   end
 end
