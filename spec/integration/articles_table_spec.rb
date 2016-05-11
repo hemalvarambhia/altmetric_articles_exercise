3da18bfe9de568da0e15cba32a25fc4a5dd5a678
Hemal <h.n.varambhia@gmail.com>
2016-05-11 12:04:46 +0100

    Example is more readable - input has been made explicit

--- spec/integration/articles_table_spec.rb
+++ spec/integration/articles_table_spec.rb
@@ -12,7 +12,14 @@ describe 'ArticlesTable' do
 
   describe '#join' do
     it 'merges the author and journal of the article together' do
-      expect(articles_table.join(journals_table, authors_table)).to(
+      path_to_csv_doc = File.join(@doc_dir, 'one_article.csv')
+      path_to_journal_csv = File.join(@doc_dir, 'one_journal.csv')
+      path_to_json_doc = File.join(@doc_dir, 'one_author.json')
+      merged_row = articles_table(path_to_csv_doc).join(
+                           journals_table(path_to_journal_csv),
+                           authors_table(path_to_json_doc))
+
+      expect(merged_row).to(
         eq( { 
           doi: DOI.new('10.1234/altmetric0'), 
           title: 'Small Wooden Chair',
@@ -23,23 +30,20 @@ describe 'ArticlesTable' do
       ) 
     end
 
-    def articles_table
-      path_to_csv_doc = File.join(@doc_dir, 'one_article.csv')
-      article_csv_doc = CSV.new(File.open(path_to_csv_doc), {headers: true})
+    def articles_table(path)
+      article_csv_doc = CSV.new(File.open(path), {headers: true})
       altmetric_file = AltmetricFile.new(article_csv_doc, ArticleCSVParser)
       ArticlesTable.from altmetric_file
     end
 
-    def journals_table
-      path_to_journal_csv = File.join(@doc_dir, 'one_journal.csv')
-      journal_csv = CSV.new(File.open(path_to_journal_csv), {headers: true})
+    def journals_table(path)
+      journal_csv = CSV.new(File.open(path), {headers: true})
       altmetric_file = AltmetricFile.new(journal_csv, JournalCSVParser)
       JournalsTable.from altmetric_file
     end
 
-    def authors_table
-      path_to_json_doc = File.join(@doc_dir, 'one_author.json')
-      authors_json_doc = AuthorJSONDoc.new(File.open(path_to_json_doc))
+    def authors_table(path)
+      authors_json_doc = AuthorJSONDoc.new(File.open(path))
       AuthorsTable.from AltmetricFile.new(authors_json_doc, AuthorJSONParser)
     end
   end
