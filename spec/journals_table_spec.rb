ce210ee29c416262aa9497c69cea276860f82397
Hemal N Varambhia <h.n.varambhia@gmail.com>
2016-05-19 15:02:07 +0100

    Made the examples more readable

--- spec/journals_table_spec.rb
+++ spec/journals_table_spec.rb
@@ -1,6 +1,39 @@
 require 'journals_table'
 require 'issn'
 describe 'Journals table' do
+  describe '#find' do
+    context 'when the table does not have a journal with the ISSN' do
+      it 'returns no title' do
+        journals_table = JournalsTable.new(
+          [
+            [ ISSN.new('5155-3789'), 'Journal of Physics A' ],
+            [ ISSN.new('8654-5652'), 'Journal of Physics B' ]
+          ]
+        )
+        
+        title = journals_table.find(ISSN.new('8766-8334'))
+
+        non_existent_title = nil
+        expect(title).to eq non_existent_title
+      end
+    end
+
+    context 'when the table has a journal with the given ISSN' do
+      it 'returns the title' do
+        journals_table = JournalsTable.new(
+          [
+            [ ISSN.new('2976-9674'), 'Chemical Physics Letters' ],
+            [ ISSN.new('6195-6091'), 'Nature' ]
+          ]
+        )
+
+        title = journals_table.find(ISSN.new('2976-9674'))
+        
+        expect(title).to(eq 'Chemical Physics Letters')
+      end
+    end
+  end
+  
   describe '.from' do
     it 'loads the table from a file' do
       csv_rows = []
@@ -21,37 +54,8 @@ describe 'Journals table' do
               [ issn, 'Journal of Nanotechnology' ]
           ]
       )
+      
       expect(journals_table.find(issn)).to eq 'Journal of Physics A'
-    end
-  end
-
-  describe '#find' do
-    context 'when the table does not have a journal with the ISSN' do
-      it 'returns no title' do
-        journals_table = JournalsTable.new(
-          [
-            [ ISSN.new('5155-3789'), 'Journal of Physics A' ],
-            [ ISSN.new('8654-5652'), 'Journal of Physics B' ]
-          ]
-        )
-
-        expect(journals_table.find(ISSN.new('8766-8334'))).to be_nil
-      end
-    end
-
-    context 'when the table has a journal with the given ISSN' do
-      it 'returns the title' do
-        journals_table = JournalsTable.new(
-          [
-            [ ISSN.new('2976-9674'), 'Chemical Physics Letters' ],
-            [ ISSN.new('6195-6091'), 'Nature' ]
-          ]
-        )
-
-        expect(journals_table.find(ISSN.new('2976-9674'))).to(
-          eq 'Chemical Physics Letters'
-        )
-      end
     end
   end
 end
