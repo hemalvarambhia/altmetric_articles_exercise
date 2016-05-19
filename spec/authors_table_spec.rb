5415230ca968cf7a6fa87e4026f2d061364d7e2a
Hemal N Varambhia <h.n.varambhia@gmail.com>
2016-05-19 15:15:21 +0100

    Made the examples more readable

--- spec/authors_table_spec.rb
+++ spec/authors_table_spec.rb
@@ -7,8 +7,9 @@ describe 'Authors table' do
 
   describe '.from' do
     it 'loads the table from a file' do
+      authors_json = []
       doc = double(:author_json_doc)
-      expect(doc).to receive(:read)
+      expect(doc).to receive(:read).and_return authors_json
 
       AuthorsTable.from doc
     end
@@ -17,34 +18,40 @@ describe 'Authors table' do
   describe '#find' do
     context 'when there are no authors of an article' do
       it 'returns no authors' do
-        required = DOI.new('10.8899/altmetric02324')
+        required = a_doi
         authors_table = AuthorsTable.new(
-            [
-                ArticleAuthor.new(name: 'No Match 1', publications: [a_doi]),
-                ArticleAuthor.new(name: 'No Match 2', publications: [a_doi])
-            ]
+          [
+            ArticleAuthor.new(name: 'No Match 1', publications: [a_doi]),
+            ArticleAuthor.new(name: 'No Match 2', publications: [a_doi])
+          ]
         )
 
-        expect(authors_table.find(required)).to eq []
+        author_of_article = authors_table.find(required)
+
+        no_authors = []
+        expect(author_of_article).to eq no_authors
       end
     end 
 
     context 'when the table contains the author of an article' do
       it 'returns the name of the author' do
-        required = DOI.new('10.1111/altmetric777')
+        required = a_doi
         authors_table = AuthorsTable.new(
           [
             ArticleAuthor.new(name: 'Author', publications: [ required ]),
             ArticleAuthor.new(name: 'No Matching', publications: [a_doi])
           ]
         )
-        expect(authors_table.find(required)).to eq ['Author']
+
+        author = authors_table.find(required)
+        
+        expect(author).to eq ['Author']
       end
     end
 
     context 'when the article is published by multiple authors' do
       it "returns all the authors' names" do
-        required = DOI.new('10.2954/altmetric9435')
+        required = a_doi
         authors_table = AuthorsTable.new(
           [
             ArticleAuthor.new(name: 'Not Author', publications: [a_doi, a_doi]),
@@ -54,8 +61,10 @@ describe 'Authors table' do
           ]
         )
 
+        authors = authors_table.find(required)
+
         expected = [ 'Main Author', 'Co-Author 1', 'Co-Author 2' ]
-        expect(authors_table.find(required)).to eq expected
+        expect(authors).to eq expected
       end
     end
   end
