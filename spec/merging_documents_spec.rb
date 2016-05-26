665644ef12120622358d2c281851cb02849bd2cd
Hemal <h.n.varambhia@gmail.com>
2016-05-26 14:26:29 +0100

    Improved the names of the local variables

--- spec/merging_documents_spec.rb
+++ spec/merging_documents_spec.rb
@@ -1,24 +1,24 @@
 describe 'merging documents and outputting the result to JSON' do
   it 'merges articles with authors and journals' do
-    article_csv = double(:articles_csv)
-    journals_csv = double(:journals_csv)
-    authors_json = double(:authors_json)
+    article_csv_doc = double(:articles_csv)
+    journal_csv_doc = double(:journals_csv)
+    author_json_doc = double(:authors_json)
     format = 'json'
-    expect(article_csv).to(
+    expect(article_csv_doc).to(
         receive(:each).and_yield(
             { doi: '10.1234/altmetric0', title: 'About Physics', issn: '8456-2422' }
         ))
-    expect(authors_json).to(
+    expect(author_json_doc).to(
         receive(:find).with('10.1234/altmetric0').and_return [ 'Author' ])
-    expect(journals_csv).to receive(:find).with('8456-2422').and_return 'Nature'
+    expect(journal_csv_doc).to receive(:find).with('8456-2422').and_return 'Nature'
 
     merged_row = {}
     if format == 'json'
-      article_csv.each do |article|
+      article_csv_doc.each do |article|
         merged_row[:doi] = article[:doi]
         merged_row[:title] = article[:title]
-        merged_row[:author] = authors_json.find(article[:doi]).join
-        merged_row[:journal] = journals_csv.find(article[:issn])
+        merged_row[:author] = author_json_doc.find(article[:doi]).join
+        merged_row[:journal] = journal_csv_doc.find(article[:issn])
         merged_row[:issn] = article[:issn]
       end
     end
