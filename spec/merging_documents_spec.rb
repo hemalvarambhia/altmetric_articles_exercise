401f0aa3ce393058e27c8ee6a474722a04536835
Hemal <h.n.varambhia@gmail.com>
2016-05-27 09:36:57 +0100

    Moved the example into a block describing the how output is formatted

--- spec/merging_documents_spec.rb
+++ spec/merging_documents_spec.rb
@@ -1,61 +1,63 @@
-describe 'merging documents and outputting the result to JSON' do
-  def merge(article_csv_doc, author_json_doc, journal_csv_doc, format)
-    rows = []
-    if format == 'json'
-      article_csv_doc.each do |article|
-        rows <<
-            {
-                doi: article[:doi],
-                title: article[:title],
-                author: author_json_doc.find(article[:doi]).join,
-                journal: journal_csv_doc.find(article[:issn]),
-                issn: article[:issn]
-            }
+describe 'merging documents' do
+  describe 'outputting the result to JSON' do
+    def merge(article_csv_doc, author_json_doc, journal_csv_doc, format)
+      rows = []
+      if format == 'json'
+        article_csv_doc.each do |article|
+          rows <<
+              {
+                  doi: article[:doi],
+                  title: article[:title],
+                  author: author_json_doc.find(article[:doi]).join,
+                  journal: journal_csv_doc.find(article[:issn]),
+                  issn: article[:issn]
+              }
+        end
       end
+
+      rows
     end
 
-    rows
-  end
+    before :each do
+      @article_csv_doc = double(:articles_csv)
+      @journal_csv_doc = double(:journals_csv)
+      @author_json_doc = double(:authors_json)
+      @format = 'json'
+    end
 
-  before :each do
-    @article_csv_doc = double(:articles_csv)
-    @journal_csv_doc = double(:journals_csv)
-    @author_json_doc = double(:authors_json)
-    @format = 'json'
-  end
+    it 'merges an article with its authors and the journal it was published in' do
+      allow(@article_csv_doc).to(
+          receive(:each).and_yield(
+              { doi: '10.1234/altmetric0', title: 'About Physics', issn: '8456-2422' }
+          ))
+      allow(@author_json_doc).to(
+          receive(:find).with('10.1234/altmetric0').and_return [ 'Author' ])
+      allow(@journal_csv_doc).to receive(:find).with('8456-2422').and_return 'Nature'
 
-  it 'merges an article with its authors and the journal it was published in' do
-    allow(@article_csv_doc).to(
-        receive(:each).and_yield(
-            { doi: '10.1234/altmetric0', title: 'About Physics', issn: '8456-2422' }
-        ))
-    allow(@author_json_doc).to(
-        receive(:find).with('10.1234/altmetric0').and_return [ 'Author' ])
-    allow(@journal_csv_doc).to receive(:find).with('8456-2422').and_return 'Nature'
+      merged_row = merge(@article_csv_doc, @author_json_doc, @journal_csv_doc, @format)
 
-    merged_row = merge(@article_csv_doc, @author_json_doc, @journal_csv_doc, @format)
+      expected = {
+          doi: '10.1234/altmetric0', title: 'About Physics', author: 'Author',
+          journal: 'Nature', issn: '8456-2422'
+      }
+      expect(merged_row).to(include(expected))
+    end
 
-    expected = {
-        doi: '10.1234/altmetric0', title: 'About Physics', author: 'Author',
-        journal: 'Nature', issn: '8456-2422'
-    }
-    expect(merged_row).to(include(expected))
-  end
+    it 'merges all articles with their authors and the journal it was published in' do
+      rows = [
+          {doi: '10.1234/altmetric1', title: 'About Chemistry', issn: '6844-2395'},
+          {doi: '10.1234/altmetric2', title: 'About Biology', issn: '5679-2344'},
+          {doi: '10.1234/altmetric3', title: 'About Biology', issn: '3141-5916'},
+      ]
+      allow(@article_csv_doc).to(
+          receive(:each).and_yield(rows[0]).and_yield(rows[1]).and_yield(rows[2]))
+      allow(@author_json_doc).to receive(:find).with(any_args).and_return an_author
+      allow(@journal_csv_doc).to receive(:find).with(any_args).and_return a_journal
 
-  it 'merges all articles with their authors and the journal it was published in' do
-    rows = [
-        {doi: '10.1234/altmetric1', title: 'About Chemistry', issn: '6844-2395'},
-        {doi: '10.1234/altmetric2', title: 'About Biology', issn: '5679-2344'},
-        {doi: '10.1234/altmetric3', title: 'About Biology', issn: '3141-5916'},
-    ]
-    allow(@article_csv_doc).to(
-        receive(:each).and_yield(rows[0]).and_yield(rows[1]).and_yield(rows[2]))
-    allow(@author_json_doc).to receive(:find).with(any_args).and_return an_author
-    allow(@journal_csv_doc).to receive(:find).with(any_args).and_return a_journal
+      merged_rows = merge(@article_csv_doc, @author_json_doc, @journal_csv_doc, @format)
 
-    merged_rows = merge(@article_csv_doc, @author_json_doc, @journal_csv_doc, @format)
-
-    expect(merged_rows.size).to eq rows.size
+      expect(merged_rows.size).to eq rows.size
+    end
   end
 
   def an_author
