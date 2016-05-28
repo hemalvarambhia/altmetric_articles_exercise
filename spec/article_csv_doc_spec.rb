068895701171df9aa4644ddbfdee1b538f43717e
Hemal N Varambhia <h.n.varambhia@gmail.com>
2016-05-28 14:39:03 +0100

    When the ISSN is missing a dash it is added in (via the parser)

--- spec/article_csv_doc_spec.rb
+++ spec/article_csv_doc_spec.rb
@@ -1,11 +1,24 @@
 require 'forwardable'
 describe 'An article CSV doc' do
+  class ArticleCSVParser
+    def self.parse row
+      issn = row[:issn]
+      corrected_issn = has_dash?(issn) ? issn : issn.insert(4, '-')
+      { doi: row[:doi], title: row[:title], issn: corrected_issn }
+    end
+
+    def self.has_dash?(issn)
+      issn.index('-')
+    end
+  end
+  
   class ArticleCSVDoc
-    extend Forwardable
-    def_delegator :@rows, :each
-       
-    def initialize rows = []
-      @rows = rows
+    def initialize row = {}
+      @row = row
+    end
+
+    def each
+      yield ArticleCSVParser.parse(@row)
     end
   end
   
@@ -14,7 +27,7 @@ describe 'An article CSV doc' do
       expected_row = {
         doi: '10.1234/altmetric1', title: 'About Physics', issn: '5463-4695'
       }
-      article_csv_doc = ArticleCSVDoc.new [expected_row]
+      article_csv_doc = ArticleCSVDoc.new expected_row
 
       expect { |b| article_csv_doc.each(&b) }
         .to yield_successive_args expected_row
@@ -24,10 +37,21 @@ describe 'An article CSV doc' do
       expected_row = {
         doi: '10.1234/altmetric2', title: 'About Chemistry', issn: '1234-5678'
       }
-      article_csv_doc = ArticleCSVDoc.new [expected_row]
+      article_csv_doc = ArticleCSVDoc.new expected_row
       
       expect { |b| article_csv_doc.each(&b) }
         .to yield_successive_args expected_row
+    end
+
+    context 'when the ISSN is missing a dash' do
+      it 'adds it in' do
+        row = { doi: nil, title: nil, issn: '12345678' }
+        article_csv_doc = ArticleCSVDoc.new row
+        
+        expected_row = { doi: nil, title: nil, issn: '1234-5678' }
+        expect { |b| article_csv_doc.each(&b) }
+          .to yield_successive_args expected_row 
+      end
     end
   end
 end
