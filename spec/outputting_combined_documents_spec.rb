0d3a637666668628f0175548df66db49141f54cb
Hemal N Varambhia <h.n.varambhia@gmail.com>
2016-05-28 18:14:33 +0100

    We now publish authors of the an article. Multiple authors are comma-separated

--- spec/outputting_combined_documents_spec.rb
+++ spec/outputting_combined_documents_spec.rb
@@ -4,9 +4,15 @@ describe 'Outputting combined documents' do
       {
         'doi' => document.read[:doi],
         'title' => document.read[:title],
-        'author' => document.read[:author].to_s,
+        'author' => comma_separated(document.read[:author]),
         'issn' => document.read[:issn]
       }
+    end
+
+    private
+
+    def comma_separated authors
+      authors.join(',')
     end
   end
 
@@ -29,7 +35,7 @@ describe 'Outputting combined documents' do
     end
 
     it 'publishes the title' do
-      a_row = { title: 'The R-matrix Method' }
+      a_row = { title: 'The R-matrix Method', author: [] }
       allow(@documents_combined).to(receive(:read).and_return a_row)
       
       output = @formatter.output_in(@required_format, @documents_combined)
@@ -37,10 +43,10 @@ describe 'Outputting combined documents' do
       expect(output['title']).to eq 'The R-matrix Method'
     end
 
-    describe 'publishing the authors' do
-      context 'when there is no author' do
+    describe 'publishing the author' do
+      context 'when there is none' do
         it 'publishes a blank' do
-          no_author = nil
+          no_author = []
           a_row = { author: no_author }
           allow(@documents_combined).to(receive(:read).and_return a_row)
           
@@ -49,10 +55,35 @@ describe 'Outputting combined documents' do
           expect(json_output['author']).to be_empty
         end
       end
+
+      context 'when there is a single author' do
+        it 'publishes their name' do
+          a_row = { author: [ 'Physicist' ] }
+          allow(@documents_combined)
+            .to(receive(:read).and_return a_row) 
+          
+          json_output = @formatter.output_in @format, @documents_combined
+          
+          expect(json_output['author']).to eq 'Physicist'
+        end
+      end
+
+      context 'when there is more than 1 author' do
+        it 'publishes their names separated by a comma' do
+          a_row = { author: ['Main Author', 'Co-Author 1', 'Co-Author 2']}
+          allow(@documents_combined)
+            .to(receive(:read).and_return a_row)
+
+          json_output = @formatter.output_in @format, @documents_combined
+          
+          expect(json_output['author'])
+            .to eq 'Main Author,Co-Author 1,Co-Author 2'
+        end
+      end
     end
 
     it 'publishes the ISSN' do
-      a_row = { issn: '1234-5678' }
+      a_row = { issn: '1234-5678', author: [] }
       allow(@documents_combined)
         .to receive(:read).and_return a_row
       
