bdf06d3396aed898759f740f180a93c94c0a2c64
Hemal N Varambhia <h.n.varambhia@gmail.com>
2016-05-28 13:19:16 +0100

    ArticleCSVRow yields any row passed to it, as long as it's just one row

--- spec/article_csv_doc_spec.rb
+++ spec/article_csv_doc_spec.rb
@@ -1,22 +1,37 @@
 describe 'An article CSV doc' do
   ArticleCSVDoc = Class.new
   class ArticleCSVDoc
-    def each
-      row = {
+    def initialize rows = {}
+      @rows = rows ||
+      {
         doi: '10.1234/altmetric1',
         title: 'About Physics',
         issn: '5463-4695'
       }
-      yield row
+    end
+    
+    def each
+      yield @rows
     end
   end
   
   describe '#each' do
     it 'yields a row to the block' do
-      article_csv_doc = ArticleCSVDoc.new
       expected_row = {
         doi: '10.1234/altmetric1', title: 'About Physics', issn: '5463-4695'
       }
+      article_csv_doc = ArticleCSVDoc.new expected_row
+
+      expect { |b| article_csv_doc.each(&b) }
+        .to yield_successive_args expected_row
+    end
+
+    it 'yields any row to the block' do
+      expected_row = {
+        doi: '10.1234/altmetric2', title: 'About Chemistry', issn: '1234-5678'
+      }
+      article_csv_doc = ArticleCSVDoc.new expected_row
+      
       expect { |b| article_csv_doc.each(&b) }
         .to yield_successive_args expected_row
     end
