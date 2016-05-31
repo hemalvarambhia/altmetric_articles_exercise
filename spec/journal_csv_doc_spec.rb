af59e750bfd8b147174e9cef8c5f0c970b256f1d
Hemal <h.n.varambhia@gmail.com>
2016-05-31 14:19:32 +0100

    Introduced anonymous and parameterised creation methods to improve clarity

--- spec/journal_csv_doc_spec.rb
+++ spec/journal_csv_doc_spec.rb
@@ -17,39 +17,38 @@ describe 'The journal csv doc' do
 
     context 'when there is a title for the given issn' do
       it 'returns the title' do
-        content = [
-            ['Nature', @issn],
-            [a_journal, generate_issn],
-            [a_journal, generate_issn]
-        ]
+        content =  some_content_including ['Phys. Rev. A', @issn]
         journal_csv_doc = JournalCSVDoc.new (content)
 
-        expect(journal_csv_doc.find(@issn)).to eq 'Nature'
+        expect(journal_csv_doc.find(@issn)).to eq 'Phys. Rev. A'
       end
 
       it 'returns the title for any given ISSN' do
-        content = [
-            ['J. Phys. B', @issn],
-            [a_journal, generate_issn],
-            [a_journal, generate_issn]
-        ]
+        content = some_content_including ['Chem. Phys Lett.', @issn]
         journal_csv_doc = JournalCSVDoc.new (content)
 
-        expect(journal_csv_doc.find(@issn)).to eq 'J. Phys. B'
+        expect(journal_csv_doc.find(@issn)).to eq 'Chem. Phys Lett.'
       end
     end
 
     context 'when there is no title for the given ISSN' do
       it 'returns no title' do
-        content = [
-            [a_journal, generate_issn],
-            [a_journal, generate_issn],
-            [a_journal, generate_issn]
-        ]
-        journal_csv_doc = JournalCSVDoc.new (content)
+        journal_csv_doc = JournalCSVDoc.new(some_content)
 
         expect(journal_csv_doc.find(@issn)).to eq ''
       end
+    end
+
+    def some_content_including(row)
+      some_content + [ row ]
+    end
+
+    def some_content
+      [
+          [a_journal, generate_issn],
+          [a_journal, generate_issn],
+          [a_journal, generate_issn]
+      ]
     end
 
     def a_journal
