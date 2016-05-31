5ebf46b850ba6468f6ce253ca6ecb5f9a27905b9
Hemal <h.n.varambhia@gmail.com>
2016-05-31 17:35:05 +0100

    When an article has multiple authors the JSON doc returns all their names

--- spec/author_json_doc_spec.rb
+++ spec/author_json_doc_spec.rb
@@ -7,8 +7,8 @@ describe 'An author JSON doc' do
     end
 
     def find doi
-      matching_author = @content.detect { |author| author['articles'].include?(doi) }
-      [ matching_author['name'] ]
+      matching_author = @content.select { |author| author['articles'].include?(doi) }
+      matching_author.collect { |author| author['name'] }
     end
   end
 
@@ -40,6 +40,29 @@ describe 'An author JSON doc' do
 
         author_of_publication = author_json_doc.find(@doi)
         expect(author_of_publication).to eq ['W Heisenberg']
+      end
+    end
+
+    context 'when more than one author published the article' do
+      it "returns every author's name" do
+        content = [
+            {
+                'name' => 'W Heisenberg',
+                'articles' => publications_including(@doi),
+            },
+            {
+                'name' => 'E Schrodinger',
+                'articles' => publications_including(@doi),
+            },
+            {
+                'name' => 'P A M Dirac',
+                'articles' => publications_including(@doi),
+            }
+        ]
+        author_json_doc = AuthorJSONDoc.new content
+
+        author_of_publication = author_json_doc.find(@doi)
+        expect(author_of_publication).to eq ['W Heisenberg', 'E Schrodinger', 'P A M Dirac']
       end
     end
   end
