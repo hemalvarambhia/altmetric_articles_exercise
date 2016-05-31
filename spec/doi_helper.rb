916c92854d63e679944f075e87a761069b82b8cf
Hemal <h.n.varambhia@gmail.com>
2016-05-31 13:27:36 +0100

    Removed unused code

--- spec/doi_helper.rb
+++ spec/doi_helper.rb
@@ -1,14 +1,5 @@
 require 'doi'
 module CreateDOI
-  def a_doi
-    registrant = Array.new(4) { rand(0..9) }.join
-    doi("10.#{registrant}/altmetric#{rand(100000000)}")
-  end
-
-  def doi(code)
-    DOI.new(code)
-  end
-
   def generate_doi
     registrant = Array.new(4) { rand(0..9) }.join
     "10.#{registrant}/altmetric#{rand(100000000)}"
