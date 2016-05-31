10e873eb7c42511edb513525852ef11c211f99d9
Hemal <h.n.varambhia@gmail.com>
2016-05-31 12:51:51 +0100

    When there is a row where the ISSN has no '-', the '-' is added

--- spec/article_csv_doc_spec.rb
+++ spec/article_csv_doc_spec.rb
@@ -4,11 +4,22 @@ describe 'An article CSV doc' do
   include CreateDOI, CreateISSN
   class ArticleCSVDoc
     def initialize rows = []
-      @rows = rows
+      @rows = rows.collect do |row|
+        { doi: row[:doi], title: row[:title], issn: correct_issn(row[:issn]) }
+      end
     end
 
     def read
       @rows
+    end
+
+    private
+
+    def correct_issn(issn)
+      dash_absent = issn.index('-').nil?
+      corrected =  dash_absent ? issn.insert(4, '-') : issn
+
+      corrected
     end
   end
   
@@ -26,6 +37,22 @@ describe 'An article CSV doc' do
 
       expect(article_csv_doc.read).to eq expected_rows
     end
+
+    describe 'when a row has an ISSN without the dash' do
+      it 'adds the dash' do
+        row_with_invalid_issn = a_row_with(issn: '12345678')
+        article_csv_doc = ArticleCSVDoc.new [ row_with_invalid_issn ]
+
+        content = article_csv_doc.read
+
+        row = content.detect {|row| row[:doi] == row_with_invalid_issn[:doi]}
+        expect(row[:issn]).to eq '1234-5678'
+      end
+    end
+  end
+
+  def a_row_with(params)
+    a_row.merge params
   end
 
   def a_row
