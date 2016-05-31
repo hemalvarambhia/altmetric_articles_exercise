73922d33fc19d8472a07903ee1b2fd6e825f71b6
Hemal <h.n.varambhia@gmail.com>
2016-05-31 13:26:55 +0100

    Removed redundant code

--- spec/issn_helper.rb
+++ spec/issn_helper.rb
@@ -1,14 +1,5 @@
 require 'issn'
 module CreateISSN
-  def an_issn
-    code = Array.new(8) { rand(0..9) }
-    issn "#{code.join}"
-  end
-
-  def issn(code)
-    ISSN.new(code)
-  end
-
   def generate_issn
     digits = Array.new(8) { rand(0..9) }
     "#{digits[0..3].join}-#{digits[4..-1].join}"
