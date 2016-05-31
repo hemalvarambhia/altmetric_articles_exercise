b7a42442e12059ebdae9f80bfe2fa11ee81e67b3
Hemal <h.n.varambhia@gmail.com>
2016-05-31 13:55:47 +0100

    When there is no title for the given ISSN, the journal csv doc returns a blank title

--- spec/journal_csv_doc_spec.rb
+++ spec/journal_csv_doc_spec.rb
@@ -8,14 +8,14 @@ describe 'The journal csv doc' do
     end
 
     def find issn
-      @journals[issn]
+      @journals.fetch(issn, '')
     end
   end
 
   describe '#find' do
-    context 'when there is a title for the given issn' do
-      before(:each) { @issn = generate_issn }
+    before(:each) { @issn = generate_issn }
 
+    context 'when there is a title for the given issn' do
       it 'returns the title' do
         journal_csv_doc = JournalCSVDoc.new ([['Nature', @issn]])
 
@@ -26,6 +26,14 @@ describe 'The journal csv doc' do
         journal_csv_doc = JournalCSVDoc.new ([['J. Phys. B', @issn]])
 
         expect(journal_csv_doc.find(@issn)).to eq 'J. Phys. B'
+      end
+    end
+
+    context 'when there is no title for the given ISSN' do
+      it 'returns no title' do
+        journal_csv_doc = JournalCSVDoc.new ([['J. Phys. B', generate_issn]])
+
+        expect(journal_csv_doc.find(@issn)).to eq ''
       end
     end
   end
