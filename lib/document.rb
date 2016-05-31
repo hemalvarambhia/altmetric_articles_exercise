df98c6dca897df6517eb3f9b80d6594aecc366f2
Hemal <h.n.varambhia@gmail.com>
2016-05-31 13:24:28 +0100

    Removed redundant code

--- lib/document.rb
+++
@@ -1,9 +0,0 @@
-require 'forwardable'
-class Document
-  extend Forwardable
-  def_delegators :@content, :<<, :include?, :[], :empty?
-
-  def initialize(articles = [])
-    @content = articles
-  end
-end
