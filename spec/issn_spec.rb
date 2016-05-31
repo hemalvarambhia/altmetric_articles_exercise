8e4b647fec03849236fae1037f6fcf09f3f7f9cc
Hemal <h.n.varambhia@gmail.com>
2016-05-31 15:56:59 +0100

    Added tests for the module that was extracted out

---
+++ spec/issn_spec.rb
@@ -0,0 +1,17 @@
+require 'issn'
+describe 'ISSN' do
+  include ISSN
+  describe 'correcting an ISSN code' do
+    context 'when it is missing a dash in the middle' do
+      it 'is added there' do
+        expect(correct_issn('12345678')).to eq '1234-5678'
+      end
+    end
+
+    context 'when the dash is already in the right place' do
+      it 'does not add another dash' do
+        expect(correct_issn('8765-4321')).to eq '8765-4321'
+      end
+    end
+  end
+end
