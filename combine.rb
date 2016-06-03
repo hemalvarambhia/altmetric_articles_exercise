8cca6ca4440a74ef05b9a497a1749f504a3b4f14
Hemal <h.n.varambhia@gmail.com>
2016-06-03 12:07:22 +0100

    New design has yielded a simpler driving script

--- combine.rb
+++ combine.rb
@@ -1,18 +1,12 @@
 $LOAD_PATH.unshift 'lib'
 require 'csv'
-require 'altmetric_file'
-require 'article_csv_parser'
-require 'articles_table'
-require 'journal_csv_parser'
-require 'journals_table'
-require 'author_json_doc'
-require 'author_json_parser'
-require 'authors_table'
-require 'combined'
-require 'json_document'
-require 'csv_document'
+require 'json'
 require 'optparse'
-
+require 'in_format'
+require 'documents_combined'
+require 'article_csv_doc'
+require 'journal_csv_doc'
+require 'author_json_doc'
 options = {}
 
 OptionParser.new do |opts|
@@ -26,28 +20,19 @@ end.parse!
 format = options[:format]
 journal_csv, articles_csv, authors_json = [ARGV[0], ARGV[1], ARGV[2]]
 
-journals_csv_doc = AltmetricFile.new(
-    CSV.new(File.open(journal_csv), {headers: true}),
-    JournalCSVParser
+journal_csv_doc = JournalCSVDoc.new(
+    CSV.new(File.open(journal_csv), headers:true, :header_converters => :symbol)
 )
-journals = JournalsTable.from(journals_csv_doc)
+articles_csv_doc = ArticleCSVDoc.new(
+    CSV.new(File.open(articles_csv), headers:true, :header_converters => :symbol)
+)
+author_json_doc = AuthorJSONDoc.new(
+    JSON.parse(File.open(authors_json).read)
+)
+documents_combined = DocumentsCombined.new(
+    articles_csv_doc,
+    journal_csv_doc,
+    author_json_doc)
 
-authors_json_doc = AuthorJSONDoc.new(File.open(authors_json))
-authors = AuthorsTable.from(
-  AltmetricFile.new(authors_json_doc, AuthorJSONParser))
-
-articles_csv_doc = AltmetricFile.new(
-    CSV.new(File.open(articles_csv), {headers: true}),
-    ArticleCSVParser)
-articles = ArticlesTable.from(articles_csv_doc)
-
-document = if format == 'json'
-             JSONDocument.new
-           else
-             CSVDocument.new
-           end
-
-combined = Combined.new(articles, journals, authors)
-combined.output_to document
-
-puts document
+format = InFormat.output(format, documents_combined)
+puts format
