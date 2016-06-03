9686c8872d8b454ba01527b5f0a6bd4d3319b60b
Hemal <h.n.varambhia@gmail.com>
2016-06-03 12:05:34 +0100

    Nested the classes, thus able to improve their names

--- lib/in_format.rb
+++ lib/in_format.rb
@@ -1,74 +1,76 @@
+require 'json'
+require 'csv'
 class InFormat
   def self.output(format, document)
     if format == 'json'
-      return InJSONFormat.new(document)
+      return JSON.new(document)
     else
-      return InCSVFormat.new(document)
+      return CSV.new(document)
     end
   end
-end
 
-require 'json'
-class InJSONFormat
-  def initialize(document)
-    @document = document
+  class JSON
+    def initialize(document)
+      @document = document
+    end
+
+    def output_in
+      @document.read.collect { |row| as_json row }
+    end
+
+    def to_s
+      JSON.pretty_generate(output_in)
+    end
+
+    private
+
+    def as_json row
+      {
+      'doi' => row[:doi],
+      'title' => row[:title],
+      'author' => comma_separated(row[:author]),
+      'journal' => row[:journal],
+      'issn' => row[:issn]
+      }
+    end
+
+    def comma_separated authors
+      authors.join(',')
+    end
   end
 
-  def output_in
-    @document.read.collect { |row| as_json row }
-  end
+  class CSV
+    def initialize(document)
+      @document = document
+    end
 
-  def to_s
-    JSON.pretty_generate(output_in)
-  end
+    def output_in
+      @document.read.collect { |row| as_csv row }
+    end
 
-  private
-
-  def as_json row
-    {
-        'doi' => row[:doi],
-        'title' => row[:title],
-        'author' => comma_separated(row[:author]),
-        'journal' => row[:journal],
-        'issn' => row[:issn]
-    }
-  end
-
-  def comma_separated authors
-    authors.join(',')
-  end
-end
-
-require 'csv'
-class InCSVFormat
-  def initialize(document)
-    @document = document
-  end
-
-  def output_in
-    @document.read.collect { |row| as_csv row }
-  end
-
-  def to_s
-    CSV.generate do |csv|
-      output_in.each do |row|
-        csv << row
+    def to_s
+      CSV.generate do |csv|
+        output_in.each do |row|
+          csv << row
+        end
       end
     end
-  end
 
-  private
+    private
 
-  def as_csv row
-    csv_row = [
-        row[:doi], row[:title], comma_separated(row[:author]),
-        row[:journal], row[:issn]
-    ]
+    def as_csv row
+      csv_row = [
+          row[:doi], row[:title], comma_separated(row[:author]),
+          row[:journal], row[:issn]
+      ]
 
-    csv_row
-  end
+      csv_row
+    end
 
-  def comma_separated authors
-    authors.join(',')
+    def comma_separated authors
+      authors.join(',')
+    end
   end
 end
+
+
