210c569d045a701a9af4a863951938a53ad69071
Hemal <h.n.varambhia@gmail.com>
2016-06-07 14:40:27 +0100

    Moved the ISSN class to a more sensible place

--- lib/issn.rb
+++ lib/issn.rb
@@ -5,4 +5,31 @@ module ISSN
 
     corrected
   end
+
+  class ISSN
+    extend Forwardable
+    def_delegators :@code, :hash
+    attr_reader :code
+
+    Malformed = Class.new(Exception)
+    def initialize(code)
+      raise Malformed.new("ISSN '#{code}' is malformed") unless code=~/^\d{4}-?\d{4}$/
+      @code = code
+      @code = code.insert(4, '-') if dash_missing?(code)
+    end
+
+    def dash_missing?(code)
+      code.scan(/-/).none?
+    end
+
+    def ==(other)
+      return code == other unless other.class == ISSN
+
+      code == other.code
+    end
+
+    def eql?(other)
+      code.eql?(other.code)
+    end
+  end
 end
--- spec/issn_spec.rb
+++ spec/issn_spec.rb
@@ -18,34 +18,6 @@ describe 'ISSN' do
 
   describe 'an ISSN' do
     require 'forwardable'
-    module ISSN
-      class ISSN
-        extend Forwardable
-        def_delegators :@code, :hash
-        attr_reader :code
-
-        Malformed = Class.new(Exception)
-        def initialize(code)
-          raise Malformed.new("ISSN '#{code}' is malformed") unless code=~/^\d{4}-?\d{4}$/
-          @code = code
-          @code = code.insert(4, '-') if dash_missing?(code)
-        end
-
-        def dash_missing?(code)
-          code.scan(/-/).none?
-        end
-
-        def ==(other)
-          return code == other unless other.class == ISSN
-
-          code == other.code
-        end
-
-        def eql?(other)
-          code.eql?(other.code)
-        end
-      end
-    end
 
     context 'when it is blank' do
       it 'is malformed' do
