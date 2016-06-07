b9facd3f95c6f7d00d4a6a8709c30940e8809caf
Hemal <h.n.varambhia@gmail.com>
2016-06-07 14:05:35 +0100

    Hide cumbersome ISSN instanciation behind a factory method

--- spec/issn_spec.rb
+++ spec/issn_spec.rb
@@ -1,4 +1,9 @@
 require 'issn'
+
+def issn(code)
+  ISSN::ISSN.new(code)
+end
+
 describe 'ISSN' do
   include ISSN
 
@@ -36,61 +41,62 @@ describe 'ISSN' do
 
     context 'when it is blank' do
       it 'is malformed' do
-        expect { ISSN::ISSN.new('') }.to raise_exception ISSN::ISSN::Malformed
+        expect { issn('') }.to raise_exception ISSN::ISSN::Malformed
       end
     end
 
     context 'when it consists of 8 numbers with a dash in the middle' do
       it 'is well-formed' do
-        expect { ISSN::ISSN.new('1234-8694') }.not_to raise_exception
+        expect { issn('1234-8694') }.not_to raise_exception
       end
     end
 
     context "when it consists of more than 8 numbers with a '-' in the middle" do
       it 'is malformed' do
-        expect { ISSN::ISSN.new('4586434-93546584') }
+        expect { issn('4586434-93546584') }
             .to raise_exception ISSN::ISSN::Malformed
       end
     end
 
     context 'when it contains any letter characters' do
       it 'is malformed' do
-        expect { ISSN::ISSN.new('9b4c8s2-954a2') }
+        expect { issn('9b4c8s2-954a2') }
             .to raise_exception ISSN::ISSN::Malformed
-        expect { ISSN::ISSN.new('1A2B3C4D-6T7D89') }
+        expect { issn('1A2B3C4D-6T7D89') }
             .to raise_exception ISSN::ISSN::Malformed
       end
     end
 
     context "when the '-' is not in the middle" do
       it 'is malformed' do
-        expect { ISSN::ISSN.new('12-346789') }.to raise_exception ISSN::ISSN::Malformed
+        expect { issn('12-346789') }.to raise_exception ISSN::ISSN::Malformed
       end
     end
 
     context "when there is anything but a '-' in the middle" do
       it 'is malformed' do
-        expect { ISSN::ISSN.new('1234/6789') }.to raise_exception ISSN::ISSN::Malformed
+        expect { issn('1234/6789') }.to raise_exception ISSN::ISSN::Malformed
       end
     end
 
     it 'stores the code' do
-      expect(ISSN::ISSN.new('6849-2347').code).to eq '6849-2347'
+      expect(issn('6849-2347').code).to eq '6849-2347'
     end
 
     context "when the dash is missing" do
       it 'is still well-formed' do
-        expect { ISSN::ISSN.new('76540442') }.not_to raise_exception
+        expect { issn('76540442') }.not_to raise_exception
       end
 
       it 'adds the dash in the correct place' do
-        expect( ISSN::ISSN.new('86756657').code).to eq '8675-6657'
+        code = '86756657'
+        expect( issn(code).code).to eq '8675-6657'
       end
     end
 
     describe '#==' do
       it 'is reflexive' do
-        issn_1 = ISSN::ISSN.new('5834-4239')
+        issn_1 = issn('5834-4239')
 
         expect(issn_1).to eq issn_1
       end
