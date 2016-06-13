171c648927e5a92d3015cbf75e06913b39e680ec
Hemal <h.n.varambhia@gmail.com>
2016-06-13 13:41:20 +0100

    We can decide the format at option parsing time via a lambda function

--- combine.rb
+++ combine.rb
@@ -13,11 +13,10 @@ OptionParser.new do |opts|
   opts.banner = "Usage: combine.rb [options]"
 
   opts.on("-f", "--format FORMAT", String, "Render articles to a particular format") do |format|
-    options[:format] = format
+    options[:format] = lambda { |document| InFormat.output(format, document) }
   end
 end.parse!
 
-format = options[:format]
 journal_csv, articles_csv, authors_json = [ARGV[0], ARGV[1], ARGV[2]]
 
 journal_csv_doc = JournalCSVDoc.new(
@@ -34,5 +33,5 @@ documents_combined = DocumentsCombined.new(
     journal_csv_doc,
     author_json_doc)
 
-format = InFormat.output(format, documents_combined)
+format = options[:format].call(documents_combined)
 puts format
