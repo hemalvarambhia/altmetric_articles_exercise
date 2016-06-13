18fe863d72d8acd8ccd7b1c4fc6ffae1e26ff28f
Hemal <h.n.varambhia@gmail.com>
2016-06-13 11:31:48 +0100

    Moved the DOI class to a more sensible place in the source code

---
+++ lib/doi.rb
@@ -0,0 +1,31 @@
+class DOI
+  attr_reader :code
+
+  def initialize(code)
+    raise Malformed.new(code) if code.nil?
+    registry = code[0..2]
+    raise Malformed.new(code) unless registry == '10.'
+    separator = code.index('/')
+    registrant = code[3..separator - 1]
+    raise Malformed.new(code) if registrant.empty?
+    item_id = code[separator + 1..-1]
+    raise Malformed.new(code) if item_id.empty?
+    @code = code
+  end
+
+  def ==(other)
+    return code == other if other.is_a?(String)
+    return false unless other.class == DOI
+    code == other.code
+  end
+
+  def to_s
+    code
+  end
+
+  class Malformed < Exception
+    def initialize(doi)
+      super "DOI '#{doi}' is malformed"
+    end
+  end
+end
--- spec/doi_spec.rb
+++ spec/doi_spec.rb
@@ -1,38 +1,5 @@
-require 'rspec'
-
+require 'doi'
 describe 'A DOI' do
-  class DOI
-    attr_reader :code
-
-    def initialize(code)
-      raise Malformed.new(code) if code.nil?
-      registry = code[0..2]
-      raise Malformed.new(code) unless registry == '10.'
-      separator = code.index('/')
-      registrant = code[3..separator - 1]
-      raise Malformed.new(code) if registrant.empty?
-      item_id = code[separator + 1..-1]
-      raise Malformed.new(code) if item_id.empty?
-      @code = code
-    end
-
-    def ==(other)
-      return code == other if other.is_a?(String)
-      return false unless other.class == DOI
-      code == other.code
-    end
-
-    def to_s
-      code
-    end
-
-    class Malformed < Exception
-      def initialize(doi)
-        super "DOI '#{doi}' is malformed"
-      end
-    end
-  end
-
   context 'when it is blank' do
     it 'is malformed' do
       expect { DOI.new('') }.to raise_exception DOI::Malformed
