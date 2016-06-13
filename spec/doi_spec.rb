0fd2234196ef740244dda96c39d4d1f4020e32d7
Hemal <h.n.varambhia@gmail.com>
2016-06-13 11:06:38 +0100

    Moved duplicate message set up to the exception class

--- spec/doi_spec.rb
+++ spec/doi_spec.rb
@@ -2,18 +2,22 @@ require 'rspec'
 
 describe 'A DOI' do
   class DOI
-    Malformed = Class.new(Exception)
+    class Malformed < Exception
+      def initialize(doi)
+        super "DOI '#{doi}' is malformed"
+      end
+    end
     attr_reader :code
 
     def initialize(code)
-      raise Malformed.new("DOI '#{code}' is malformed") if code.nil?
+      raise Malformed.new(code) if code.nil?
       registry = code[0..2]
-      raise Malformed.new("DOI '#{code}' is malformed") unless registry == '10.'
+      raise Malformed.new(code) unless registry == '10.'
       separator = code.index('/')
       registrant = code[3..separator - 1]
-      raise Malformed.new("DOI '#{code}' is malformed") if registrant.empty?
+      raise Malformed.new(code) if registrant.empty?
       item_id = code[separator + 1..-1]
-      raise Malformed.new("DOI '#{code}' is malformed") if item_id.empty?
+      raise Malformed.new(code) if item_id.empty?
       @code = code
     end
   end
