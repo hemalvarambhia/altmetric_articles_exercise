41efb0b0b4ece852ba175413f296d1cb36c558d6
Hemal <h.n.varambhia@gmail.com>
2016-06-13 10:41:24 +0100

    When the DOI starts with '10.', it is well formed

--- spec/doi_spec.rb
+++ spec/doi_spec.rb
@@ -5,12 +5,19 @@ describe 'A DOI' do
     Malformed = Class.new(Exception)
 
     def initialize(code)
-      raise Malformed.new("DOI '#{code}' is malformed")
+      raise Malformed.new("DOI '#{code}' is malformed") if code.empty?
     end
   end
+
   context 'when it is blank' do
     it 'is malformed' do
       expect { DOI.new('') }.to raise_exception DOI::Malformed
+    end
+  end
+
+  context "when it starts with '10.'" do
+    it 'is well-formed' do
+      expect { DOI.new('10.1234/altmetric032') }.not_to raise_exception
     end
   end
 end
